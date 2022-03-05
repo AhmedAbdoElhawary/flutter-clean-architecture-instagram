04881b969efc18c69cf496b7d1fd52cf70ac56d0
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-03-06 00:46:34 +0200

    change the primary color

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'instagram',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
       home: const MainScreen(),
     );
