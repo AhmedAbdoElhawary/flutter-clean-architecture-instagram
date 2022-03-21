bb939b2e09bed833f748e5b3b9010838693cba62
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-03-21 14:49:02 +0200

    add updateUserInfo , uploadProfileImage

--- lib/data/repositories/firestore_user_repo_impl.dart
+++ lib/data/repositories/firestore_user_repo_impl.dart
@@ -1,6 +1,6 @@
-import 'package:instegram/domain/entities/user_personal_info.dart';
+import 'dart:io';
 
-import '../../domain/entities/new_user_info.dart';
+import 'package:instegram/data/models/user_personal_info.dart';
 import '../../domain/repositories/firestore_user_repo.dart';
 import '../datasourses/remote/firebase_user_info.dart';
 
@@ -18,6 +18,28 @@ class FirebaseUserRepoImpl implements FirestoreUserRepository {
   Future<UserPersonalInfo> getPersonalInfo(String userId) async {
     try {
       return await FirestoreUsers.getUserInfo(userId);
+    } catch (e) {
+      return Future.error(e.toString());
+    }
+  }
+
+  @override
+  Future<UserPersonalInfo> updateUserInfo(
+      UserPersonalInfo updatedUserInfo) async {
+    try {
+      await FirestoreUsers.updateUserInfo(updatedUserInfo);
+      return getPersonalInfo(updatedUserInfo.userId);
+    } catch (e) {
+      return Future.error(e.toString());
+    }
+  }
+
+  @override
+  Future<String> uploadProfileImage({required File photo, required String userId}) async {
+    try {
+      String imageUrl=await FirestoreUsers.uploadImage(photo);
+     await FirestoreUsers.updateProfileImage(imageUrl: imageUrl, userId: userId);
+      return imageUrl;
     } catch (e) {
       return Future.error(e.toString());
     }
