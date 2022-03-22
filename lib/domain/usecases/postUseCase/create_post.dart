e1364d8201ad0600039bcd7084e99710850bf102
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-03-22 20:49:06 +0200

    add  CreatePostUseCase

---
+++ lib/domain/usecases/postUseCase/create_post.dart
@@ -0,0 +1,14 @@
+import '../../../core/usecase/usecase.dart';
+import '../../repositories/post_repository.dart';
+
+class CreatePostUseCase implements UseCase<String, List> {
+  final FirestorePostRepository _createPostRepository;
+
+  CreatePostUseCase(this._createPostRepository);
+
+  @override
+  Future<String> call({required List params}) {
+    return _createPostRepository.createPost(
+        postInfo: params[0], commentInfo: params[1], photo: params[2]);
+  }
+}
