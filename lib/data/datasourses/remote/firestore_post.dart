fffbe63952e25ff7651fef582dc98433769dfb88
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-03-26 02:16:37 +0200

    add getAllPostsInfo methods and getPostsInfo

--- lib/data/datasourses/remote/firestore_post.dart
+++ lib/data/datasourses/remote/firestore_post.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:instegram/data/datasourses/remote/firestore_user_info.dart';
 import 'package:instegram/data/models/comment.dart';
 import 'package:instegram/data/models/post.dart';
+import 'package:instegram/data/models/user_personal_info.dart';
 
 class FirestorePost {
   static final _firestorePostCollection =
@@ -28,16 +30,38 @@ class FirestorePost {
         .update({'commentUid': commentRef.id});
   }
 
-  static Future<List<Post>> getPostInfo(List<String> postIds) async {
+  static Future<List<Post>> getPostsInfo(List<dynamic> postsIds) async {
     List<Post> postsInfo = [];
-    for (int i = 0; i < postIds.length; i++) {
-      DocumentSnapshot<Object?> snap =
-          await _firestorePostCollection.doc(postIds[i]).get();
+    for (int i = 0; i < postsIds.length; i++) {
+      DocumentSnapshot<Map<String, dynamic>> snap =
+          await _firestorePostCollection.doc(postsIds[i]).get();
       if (snap.exists) {
-        postsInfo.add(Post.fromSnap(snap));
+        Post postReformat = Post.fromJson(snap);
+        UserPersonalInfo publisherInfo =
+            await FirestoreUser.getUserInfo(postReformat.publisherId);
+        postReformat.publisherInfo = publisherInfo;
+        postsInfo.add(postReformat);
+      } else {
+        return Future.error("the post not exist !");
       }
-      return Future.error("the post not exist !");
     }
     return postsInfo;
+  }
+
+  static Future<List<Post>> getAllPostsInfo() async {
+    List<Post> allData = [];
+    QuerySnapshot<Map<String, dynamic>> snap =
+        await _firestorePostCollection.get();
+
+    for (int i = 0; i < snap.docs.length; i++) {
+      QueryDocumentSnapshot<Map<String, dynamic>> doc = snap.docs[i];
+      Post postReformat = Post.fromSnap(doc);
+      UserPersonalInfo publisherInfo =
+          await FirestoreUser.getUserInfo(postReformat.publisherId);
+      postReformat.publisherInfo = publisherInfo;
+
+      allData.add(postReformat);
+    }
+    return allData;
   }
 }
