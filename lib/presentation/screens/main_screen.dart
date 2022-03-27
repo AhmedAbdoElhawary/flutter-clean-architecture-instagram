f710798aa0d94bdb898bb3f3ef0c8eb865dae60d
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-03-27 18:57:49 +0200

    add CupertinoTabScaffold as navigation bar

--- lib/presentation/screens/main_screen.dart
+++ lib/presentation/screens/main_screen.dart
@@ -1,73 +1,102 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:instegram/presentation/screens/profile_page.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instegram/presentation/cubit/firestoreUserInfoCubit/user_info_cubit.dart';
+import 'package:instegram/presentation/pages/all_user_posts_page.dart';
+import 'package:instegram/presentation/pages/profile_page.dart';
 import '../pages/home_page.dart';
 
 class MainScreen extends StatefulWidget {
-  const MainScreen({Key? key}) : super(key: key);
+  final String userId;
+  const MainScreen(this.userId, {Key? key}) : super(key: key);
 
   @override
   State<MainScreen> createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
 
   static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static final List<Widget> _screenOptions = [
-    const HomeScreen(),
-    const Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    const Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-    const Text(
-      'Index 3: School',
-      style: optionStyle,
-    ),
-    ProfilePage()
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Container(child: _screenOptions[_selectedIndex])),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.fixed,
-        unselectedItemColor: Colors.black,
-        items: [
+
+    return CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(items: [
           navigationBarItem("house_white.svg"),
           navigationBarItem("search.svg"),
           navigationBarItem("video.svg"),
           navigationBarItem("shop_white.svg"),
-          const BottomNavigationBarItem(
-            icon: CircleAvatar(
-                radius: 13,
-                backgroundColor: Colors.black12,
-                // ignore: todo
-                //TODO : here put the personal picture of the user
-                child: Icon(Icons.person, color: Colors.white)),
+          BottomNavigationBarItem(
+            icon:
+                BlocBuilder<FirestoreUserInfoCubit, FirestoreGetUserInfoState>(
+                    builder: (context, state) {
+              if (state is CubitUserLoaded) {
+                return CircleAvatar(
+                    radius: 14,
+                    backgroundColor: Colors.black12,
+                    child: ClipOval(
+                      child: state.userPersonalInfo.profileImageUrl.isEmpty
+                          ? const Icon(Icons.person, color: Colors.white)
+                          : Image.network(
+                              state.userPersonalInfo.profileImageUrl),
+                    ));
+              } else if (state is CubitGetUserInfoFailed) {
+                return Container();
+              } else {
+                return const ClipOval(child: CircularProgressIndicator());
+              }
+            }),
             label: '',
           ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.black,
-        onTap: _onItemTapped,
-      ),
-    );
+        ]),
+        tabBuilder: (context, index) {
+          switch (index) {
+            case 0:
+              return CupertinoTabView(
+                builder: (context) =>
+                    CupertinoPageScaffold(child: HomeScreen(widget.userId)),
+              );
+            case 1:
+              return CupertinoTabView(
+                builder: (context) =>
+                    const CupertinoPageScaffold(child: AllUserPostPage()),
+              );
+            case 2:
+              return CupertinoTabView(
+                builder: (context) => const CupertinoPageScaffold(
+                  child: Text(
+                    "widget.userInfo.email",
+                    style: optionStyle,
+                  ),
+                ),
+              );
+            case 3:
+              return CupertinoTabView(
+                builder: (context) => CupertinoPageScaffold(
+                  child: Center(
+                    child: TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      HomeScreen(widget.userId)));
+                        },
+                        child: const Text("Click here mother f***k")),
+                  ),
+                ),
+              );
+            default:
+              return CupertinoTabView(
+                builder: (context) =>
+                    CupertinoPageScaffold(child: ProfilePage(widget.userId)),
+              );
+          }
+        });
   }
 
   BottomNavigationBarItem navigationBarItem(String fileName) {
