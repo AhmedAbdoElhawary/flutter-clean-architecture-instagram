59f18989dad742824310a77d2a40873b78bd94d1
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-03-27 18:58:19 +0200

    add PostState

---
+++ lib/presentation/cubit/postInfoCubit/post_state.dart
@@ -0,0 +1,24 @@
+part of '../postInfoCubit/post_cubit.dart';
+
+abstract class PostState {}
+
+class CubitInitial extends PostState {}
+
+class CubitPostLoading extends PostState {}
+
+class CubitPostLoaded extends PostState {}
+
+class CubitPostsInfoLoaded extends PostState {
+  List<Post> postsInfo;
+
+  CubitPostsInfoLoaded(this.postsInfo);
+}
+class CubitAllPostsLoaded extends PostState {
+  List<Post> allPostInfo;
+  CubitAllPostsLoaded(this.allPostInfo);
+}
+
+class CubitPostFailed extends PostState {
+  final String error;
+  CubitPostFailed(this.error);
+}
