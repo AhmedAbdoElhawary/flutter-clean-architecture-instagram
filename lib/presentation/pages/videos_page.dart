4ba5dde2669d96c9208d8cbe4eba7d5482817313
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-07 23:04:36 +0200

    create VideosPage

---
+++ lib/presentation/pages/videos_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VideosPage extends StatelessWidget {
+  const VideosPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("It's under work !"),),
+    );
+  }
+}
