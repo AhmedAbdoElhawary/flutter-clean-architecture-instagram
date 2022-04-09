4612a7ea0e74f19ce9a95237b15be6afc682d582
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-09 23:31:53 +0200

    create GetFollowersAndFollowingsUseCase

--- lib/domain/usecases/firestoreUserUseCase/get_followers_and_followings_usecase.dart
+++ lib/domain/usecases/firestoreUserUseCase/get_followers_and_followings_usecase.dart
@@ -2,14 +2,14 @@ import 'package:instegram/data/models/specific_users_info.dart';
 import '../../../core/usecase/usecase.dart';
 import '../../repositories/user_repository.dart';
 
-class GetSpecificUsersUseCase
-    implements UseCaseTwoParams<SpecificUsersInfo, List<dynamic>,List<dynamic>> {
+class GetFollowersAndFollowingsUseCase
+    implements UseCaseTwoParams<FollowersAndFollowingsInfo, List<dynamic>,List<dynamic>> {
   final FirestoreUserRepository _fireStoreUserRepository;
 
-  GetSpecificUsersUseCase(this._fireStoreUserRepository);
+  GetFollowersAndFollowingsUseCase(this._fireStoreUserRepository);
 
   @override
-  Future<SpecificUsersInfo> call({required List<dynamic> paramsOne,required List<dynamic> paramsTwo}) {
-    return _fireStoreUserRepository.getSpecificUsersInfo(followersIds: paramsOne,followingsIds: paramsTwo);
+  Future<FollowersAndFollowingsInfo> call({required List<dynamic> paramsOne,required List<dynamic> paramsTwo}) {
+    return _fireStoreUserRepository.getFollowersAndFollowingsInfo(followersIds: paramsOne,followingsIds: paramsTwo);
   }
 }
