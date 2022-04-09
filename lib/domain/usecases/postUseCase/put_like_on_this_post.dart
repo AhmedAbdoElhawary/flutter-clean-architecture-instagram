911fca317f046864504bcd9fa8d01df573560c3b
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-09 23:33:23 +0200

    create PutLikeOnThisPostUseCase

---
+++ lib/domain/usecases/postUseCase/put_like_on_this_post.dart
@@ -0,0 +1,15 @@
+import 'package:instegram/domain/repositories/post_repository.dart';
+import '../../../core/usecase/usecase.dart';
+
+class PutLikeOnThisPostUseCase
+    implements UseCaseTwoParams<void, String, String> {
+  final FirestorePostRepository _putLikeOnThisPostRepository;
+
+  PutLikeOnThisPostUseCase(this._putLikeOnThisPostRepository);
+
+  @override
+  Future<void> call({required String paramsOne, required String paramsTwo}) {
+    return _putLikeOnThisPostRepository.putLikeOnThisPost(
+        postId: paramsOne, userId: paramsTwo);
+  }
+}
