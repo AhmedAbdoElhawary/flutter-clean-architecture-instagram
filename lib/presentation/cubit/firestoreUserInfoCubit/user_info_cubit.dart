020f08a674c0b3bea91fd28a83e3023907501de6
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-10 19:20:33 +0200

    delete getSpecificUsersInfo

--- lib/presentation/cubit/firestoreUserInfoCubit/user_info_cubit.dart
+++ lib/presentation/cubit/firestoreUserInfoCubit/user_info_cubit.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:instegram/core/constant.dart';
 import 'package:instegram/data/models/user_personal_info.dart';
-import 'package:instegram/domain/usecases/firestoreUserUseCase/get_specific_users_usecase.dart';
 import '../../../domain/usecases/firestoreUserUsecase/get_user_info_usecase.dart';
 import '../../../domain/usecases/firestoreUserUsecase/update_user_info.dart';
 import '../../../domain/usecases/firestoreUserUseCase/upload_profile_image_usecase.dart';
@@ -11,21 +9,25 @@ part 'user_info_state.dart';
 
 class FirestoreUserInfoCubit extends Cubit<FirestoreGetUserInfoState> {
   GetUserInfoUseCase getUserInfoUseCase;
-  final GetSpecificUsersUseCase _getSpecificUsersUseCase;
   UpdateUserInfoUseCase updateUserInfoUseCase;
   UploadProfileImageUseCase uploadImageUseCase;
   UserPersonalInfo? myPersonalInfo;
+
   UserPersonalInfo? userInfo;
 
   FirestoreUserInfoCubit(this.getUserInfoUseCase, this.updateUserInfoUseCase,
-      this._getSpecificUsersUseCase, this.uploadImageUseCase)
+      this.uploadImageUseCase)
       : super(CubitInitial());
 
   static FirestoreUserInfoCubit get(BuildContext context) =>
       BlocProvider.of(context);
 
-  Future<void> getUserInfo(String userId, bool isThatMyPersonalId) async {
+  void initializeTheCubit() => emit(CubitInitial());
+
+  Future<UserPersonalInfo?> getUserInfo(
+      String userId, bool isThatMyPersonalId) async {
     emit(CubitUserLoading());
+    UserPersonalInfo? myInfo;
 
     await getUserInfoUseCase.call(params: userId).then((userInfo) {
       if (isThatMyPersonalId) {
@@ -34,22 +36,12 @@ class FirestoreUserInfoCubit extends Cubit<FirestoreGetUserInfoState> {
       } else {
         this.userInfo = userInfo;
         emit(CubitUserLoaded(userInfo));
-
       }
-
+      myInfo = userInfo;
     }).catchError((e) {
       emit(CubitGetUserInfoFailed(e.toString()));
     });
-  }
-
-  Future<void> getSpecificUsersInfo(List<dynamic> usersIds) async {
-    emit(CubitUserLoading());
-
-    await _getSpecificUsersUseCase.call(params: usersIds).then((userInfo) {
-      emit(CubitSpecificUsersLoaded(userInfo));
-    }).catchError((e) {
-      emit(CubitGetUserInfoFailed(e.toString()));
-    });
+    return myInfo;
   }
 
   Future<UserPersonalInfo?> updateUserInfo(
