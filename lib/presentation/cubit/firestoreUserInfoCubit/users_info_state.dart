0678178b52b4a22f9d1b5c3b500055fe24224ec4
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-10 19:07:33 +0200

    create UsersInfoState

---
+++ lib/presentation/cubit/firestoreUserInfoCubit/users_info_state.dart
@@ -0,0 +1,24 @@
+part of 'users_info_cubit.dart';
+
+abstract class UsersInfoState {}
+
+class UsersInfoInitial extends UsersInfoState {}
+
+class CubitFollowersAndFollowingsLoading extends UsersInfoState {}
+
+class CubitFollowersAndFollowingsLoaded extends UsersInfoState {
+  FollowersAndFollowingsInfo followersAndFollowingsInfo;
+
+  CubitFollowersAndFollowingsLoaded(this.followersAndFollowingsInfo);
+}
+
+class CubitGettingSpecificUsersLoaded extends UsersInfoState {
+  List<UserPersonalInfo> specificUsersInfo;
+
+  CubitGettingSpecificUsersLoaded(this.specificUsersInfo);
+}
+
+class CubitGettingSpecificUsersFailed extends UsersInfoState {
+  final String error;
+  CubitGettingSpecificUsersFailed(this.error);
+}
