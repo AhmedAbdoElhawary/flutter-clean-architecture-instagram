977c4a492686cf98dd6e3b4166e68226c4b840f1
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-10 19:07:23 +0200

    create UsersInfoCubit

---
+++ lib/presentation/cubit/firestoreUserInfoCubit/users_info_cubit.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instegram/data/models/specific_users_info.dart';
+import 'package:instegram/domain/usecases/firestoreUserUseCase/get_followers_and_followings_usecase.dart';
+import 'package:instegram/domain/usecases/firestoreUserUseCase/get_specific_users_usecase.dart';
+import '../../../data/models/user_personal_info.dart';
+
+part 'users_info_state.dart';
+
+class UsersInfoCubit extends Cubit<UsersInfoState> {
+  final GetFollowersAndFollowingsUseCase getFollowersAndFollowingsUseCase;
+  final GetSpecificUsersUseCase getSpecificUsersUseCase;
+
+  UsersInfoCubit(
+      this.getFollowersAndFollowingsUseCase, this.getSpecificUsersUseCase)
+      : super(UsersInfoInitial());
+  static UsersInfoCubit get(BuildContext context) => BlocProvider.of(context);
+
+  Future<void> getFollowersAndFollowingsInfo(
+      {required List<dynamic> followersIds,
+      required List<dynamic> followingsIds}) async {
+    emit(CubitFollowersAndFollowingsLoading());
+    await getFollowersAndFollowingsUseCase
+        .call(paramsOne: followersIds, paramsTwo: followingsIds)
+        .then((specificUsersInfo) {
+      emit(CubitFollowersAndFollowingsLoaded(specificUsersInfo));
+    }).catchError((e) {
+      emit(CubitGettingSpecificUsersFailed(e.toString()));
+    });
+  }
+
+  Future<void> getSpecificUsersInfo({required List<dynamic> usersIds}) async {
+    emit(CubitFollowersAndFollowingsLoading());
+    await getSpecificUsersUseCase.call(params: usersIds).then((usersIds) {
+      emit(CubitGettingSpecificUsersLoaded(usersIds));
+    }).catchError((e) {
+      emit(CubitGettingSpecificUsersFailed(e.toString()));
+    });
+  }
+}
