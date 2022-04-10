396596f62d4f4dbeb5ceced09b98ad5ddffff414
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-10 19:26:23 +0200

    change the of featching data

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:instegram/data/models/post.dart';
 import 'package:instegram/presentation/cubit/postInfoCubit/post_cubit.dart';
+import 'package:instegram/presentation/cubit/postInfoCubit/specific_users_posts_cubit.dart';
 import 'package:instegram/presentation/widgets/custom_app_bar.dart';
 import 'package:instegram/presentation/widgets/post_list_view.dart';
 import 'package:instegram/presentation/widgets/smart_refresher.dart';
@@ -12,15 +13,8 @@ import '../widgets/circle_avatar_of_profile_image.dart';
 
 class HomeScreen extends StatefulWidget {
   final String userId;
-  List postsInfo;
-  final bool isThatMyPosts;
 
-  HomeScreen(
-      {Key? key,
-      required this.isThatMyPosts,
-      required this.userId,
-      required this.postsInfo})
-      : super(key: key);
+  const HomeScreen({Key? key, required this.userId}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -30,47 +24,51 @@ class _HomeScreenState extends State<HomeScreen>
     with AutomaticKeepAliveClientMixin, TickerProviderStateMixin {
   bool rebuild = true;
   bool loadingPosts = true;
-  bool loadingStories = true;
+  bool reLoadData = false;
   UserPersonalInfo? personalInfo;
 
-  Future<void> getData(bool reload) async {
+  Future<void> getData(
+      ) async {
+    reLoadData = false;
     personalInfo =
-        BlocProvider.of<FirestoreUserInfoCubit>(context, listen: false)
-            .myPersonalInfo;
-    setState(() {
-      loadingStories = false;
-    });
+        await BlocProvider.of<FirestoreUserInfoCubit>(context, listen: false)
+            .getUserInfo(widget.userId, true);
 
-    if (widget.isThatMyPosts) {
-      widget.postsInfo = personalInfo!.posts +
-          personalInfo!.followedPeople +
-          personalInfo!.followerPeople;
-      PostCubit postCubit = PostCubit.get(context);
-      await postCubit.getPostsInfo(widget.postsInfo, widget.isThatMyPosts);
+    List usersIds = personalInfo!.followedPeople + personalInfo!.followerPeople;
+    List usersPostsIds =
+        await BlocProvider.of<SpecificUsersPostsCubit>(context, listen: false)
+            .getSpecificUsersPostsInfo(usersIds: usersIds);
+    List postsIds = usersPostsIds+personalInfo!.posts;
+
+    PostCubit postCubit = PostCubit.get(context);
+    List<Post>? s =
+        await postCubit.getPostsInfo(postIds: postsIds, isThatForMyPosts: true);
+    for (int i = 0; i < s!.length; i++) {
     }
-    if (!reload) loadingPosts = false;
+
     Future.delayed(Duration.zero, () {
       setState(() {});
     });
+    reLoadData = true;
   }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    rebuild = true;
 
     final mediaQuery = MediaQuery.of(context);
     final bodyHeight = mediaQuery.size.height -
         AppBar().preferredSize.height -
         mediaQuery.padding.top;
     if (rebuild) {
-      getData(false);
-      rebuild = false;
+      getData(
+          );
+    rebuild = false;
     }
     return Scaffold(
       appBar: customAppBar(),
       body: SmarterRefresh(
-        onRefreshData: getData(true),
+        onRefreshData: getData,
         smartRefresherChild: blocBuilder(bodyHeight),
       ),
     );
@@ -78,13 +76,21 @@ class _HomeScreenState extends State<HomeScreen>
 
   BlocBuilder<PostCubit, PostState> blocBuilder(double bodyHeight) {
     return BlocBuilder<PostCubit, PostState>(
-      buildWhen: (previous, current) => (previous != current &&
-          ((current is CubitMyPostsInfoLoaded && widget.isThatMyPosts) ||
-              (current is CubitPostsInfoLoaded && !widget.isThatMyPosts))),
+      buildWhen: (previous, current) {
+        if (reLoadData &&
+            previous == current &&
+            current is CubitMyPersonalPostsLoaded) {
+          reLoadData = false;
+          return true;
+        }
+
+        if (current is CubitMyPersonalPostsLoaded) {
+          return true;
+        }
+        return false;
+      },
       builder: (BuildContext context, PostState state) {
-        if (state is CubitMyPostsInfoLoaded && widget.isThatMyPosts) {
-          return columnOfWidgets(bodyHeight, state.postsInfo);
-        } else if (state is CubitPostsInfoLoaded && !widget.isThatMyPosts) {
+        if (state is CubitMyPersonalPostsLoaded) {
           return columnOfWidgets(bodyHeight, state.postsInfo);
         } else if (state is CubitPostFailed) {
           ToastShow.toastStateError(state);
@@ -113,14 +119,8 @@ class _HomeScreenState extends State<HomeScreen>
   }
 
   Widget posts(List<Post> postsInfo) {
-    if (loadingPosts) {
-      return const Center(
-        child:
-            CircularProgressIndicator(strokeWidth: 1.5, color: Colors.black54),
-      );
-    } else {
-      return CustomPostListView(postsInfo: postsInfo);
-    }
+  
+    return CustomPostListView(postsInfo: postsInfo);
   }
 
   Widget storiesLines(double bodyHeight, List<Post> postsInfo) {
@@ -148,5 +148,5 @@ class _HomeScreenState extends State<HomeScreen>
   }
 
   @override
-  bool get wantKeepAlive => true;
+  bool get wantKeepAlive => false;
 }
