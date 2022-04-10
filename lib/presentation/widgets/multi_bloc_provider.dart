4a2523c2af8b6341925b8f4c1a38f0bf2f73d40c
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-10 19:10:55 +0200

    add some cubits

--- lib/presentation/widgets/multi_bloc_provider.dart
+++ lib/presentation/widgets/multi_bloc_provider.dart
@@ -1,37 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instegram/presentation/cubit/commentsInfo/comments_info_cubit.dart';
+import 'package:instegram/presentation/cubit/commentsInfo/post_likes/comment_likes_cubit.dart';
 import 'package:instegram/presentation/cubit/firebaseAuthCubit/firebase_auth_cubit.dart';
 import 'package:instegram/presentation/cubit/firestoreUserInfoCubit/add_new_user_cubit.dart';
 import 'package:instegram/presentation/cubit/firestoreUserInfoCubit/user_info_cubit.dart';
+import 'package:instegram/presentation/cubit/firestoreUserInfoCubit/users_info_cubit.dart';
+import 'package:instegram/presentation/cubit/followCubit/follow_cubit.dart';
+import 'package:instegram/presentation/cubit/postInfoCubit/postLikes/post_likes_cubit.dart';
+import 'package:instegram/presentation/cubit/postInfoCubit/specific_users_posts_cubit.dart';
 import '../../injector.dart';
 import '../cubit/firebaseAuthCubit/firebase_auth_cubit.dart';
 import '../cubit/postInfoCubit/post_cubit.dart';
 
 // ignore: must_be_immutable
-class MultiBloc extends StatelessWidget{
+class MultiBloc extends StatelessWidget {
   Widget materialApp;
 
-  MultiBloc(this.materialApp,{Key? key}) : super(key: key);
+  MultiBloc(this.materialApp, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  MultiBlocProvider(
-        providers: [
-          BlocProvider<FirebaseAuthCubit>(
-            create: (context) => injector<FirebaseAuthCubit>(),
-          ),
-          BlocProvider<FirestoreUserInfoCubit>(
-            create: (context) => injector<FirestoreUserInfoCubit>(),
-          ),
-          BlocProvider<FirestoreAddNewUserCubit>(
-            create: (context) => injector<FirestoreAddNewUserCubit>(),
-          ),
-          BlocProvider<PostCubit>(
-            create: (context) => injector<PostCubit>()..getAllPostInfo(),
-          ),
-
-        ],
-        child: materialApp
-    );
+    return MultiBlocProvider(providers: [
+      BlocProvider<FirebaseAuthCubit>(
+        create: (context) => injector<FirebaseAuthCubit>(),
+      ),
+      BlocProvider<FirestoreUserInfoCubit>(
+        create: (context) => injector<FirestoreUserInfoCubit>(),
+      ),
+      BlocProvider<FirestoreAddNewUserCubit>(
+        create: (context) => injector<FirestoreAddNewUserCubit>(),
+      ),
+      BlocProvider<PostCubit>(
+        create: (context) => injector<PostCubit>()..getAllPostInfo(),
+      ),
+      BlocProvider<FollowCubit>(
+        create: (context) => injector<FollowCubit>(),
+      ),
+      BlocProvider<UsersInfoCubit>(
+        create: (context) => injector<UsersInfoCubit>(),
+      ),
+      BlocProvider<SpecificUsersPostsCubit>(
+        create: (context) => injector<SpecificUsersPostsCubit>(),
+      ),
+      BlocProvider<PostLikesCubit>(
+        create: (context) => injector<PostLikesCubit>(),
+      ),
+      BlocProvider<CommentsInfoCubit>(
+        create: (context) => injector<CommentsInfoCubit>(),
+      ),
+      BlocProvider<CommentLikesCubit>(
+        create: (context) => injector<CommentLikesCubit>(),
+      ),
+    ], child: materialApp);
   }
 }
