bbfebe484e8627c1c320a0e92a3c807879fa0be7
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-13 04:59:33 +0200

    add updateUserPostsInfo

--- lib/presentation/cubit/firestoreUserInfoCubit/user_info_cubit.dart
+++ lib/presentation/cubit/firestoreUserInfoCubit/user_info_cubit.dart
@@ -2,20 +2,22 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:instegram/data/models/user_personal_info.dart';
-import '../../../domain/usecases/firestoreUserUsecase/get_user_info_usecase.dart';
-import '../../../domain/usecases/firestoreUserUsecase/update_user_info.dart';
-import '../../../domain/usecases/firestoreUserUseCase/upload_profile_image_usecase.dart';
+import 'package:instegram/domain/usecases/firestoreUserUseCase/add_post_to_user.dart';
+import 'package:instegram/domain/usecases/firestoreUserUseCase/getUserInfo/get_user_info_usecase.dart';
+import 'package:instegram/domain/usecases/firestoreUserUseCase/update_user_info.dart';
+import 'package:instegram/domain/usecases/firestoreUserUseCase/upload_profile_image_usecase.dart';
 part 'user_info_state.dart';
 
 class FirestoreUserInfoCubit extends Cubit<FirestoreGetUserInfoState> {
   GetUserInfoUseCase getUserInfoUseCase;
   UpdateUserInfoUseCase updateUserInfoUseCase;
   UploadProfileImageUseCase uploadImageUseCase;
+  AddPostToUserUseCase addPostToUserUseCase;
   UserPersonalInfo? myPersonalInfo;
 
   UserPersonalInfo? userInfo;
 
-  FirestoreUserInfoCubit(this.getUserInfoUseCase, this.updateUserInfoUseCase,
+  FirestoreUserInfoCubit(this.getUserInfoUseCase, this.updateUserInfoUseCase,this.addPostToUserUseCase,
       this.uploadImageUseCase)
       : super(CubitInitial());
 
@@ -24,11 +26,9 @@ class FirestoreUserInfoCubit extends Cubit<FirestoreGetUserInfoState> {
 
   void initializeTheCubit() => emit(CubitInitial());
 
-  Future<UserPersonalInfo?> getUserInfo(
+  Future<void> getUserInfo(
       String userId, bool isThatMyPersonalId) async {
     emit(CubitUserLoading());
-    UserPersonalInfo? myInfo;
-
     await getUserInfoUseCase.call(params: userId).then((userInfo) {
       if (isThatMyPersonalId) {
         myPersonalInfo = userInfo;
@@ -37,14 +37,12 @@ class FirestoreUserInfoCubit extends Cubit<FirestoreGetUserInfoState> {
         this.userInfo = userInfo;
         emit(CubitUserLoaded(userInfo));
       }
-      myInfo = userInfo;
     }).catchError((e) {
       emit(CubitGetUserInfoFailed(e.toString()));
     });
-    return myInfo;
   }
 
-  Future<UserPersonalInfo?> updateUserInfo(
+  Future<void> updateUserInfo(
       UserPersonalInfo updatedUserInfo) async {
     emit(CubitUserLoading());
     await updateUserInfoUseCase.call(params: updatedUserInfo).then((userInfo) {
@@ -53,9 +51,17 @@ class FirestoreUserInfoCubit extends Cubit<FirestoreGetUserInfoState> {
     }).catchError((e) {
       emit(CubitGetUserInfoFailed(e.toString()));
     });
-    return myPersonalInfo;
   }
-
+  Future<void> updateUserPostsInfo(
+      {required String userId,required String postId}) async {
+    emit(CubitUserLoading());
+    await addPostToUserUseCase.call(paramsOne: userId,paramsTwo:postId ).then((userInfo) {
+      myPersonalInfo = userInfo;
+      emit(CubitMyPersonalInfoLoaded(userInfo));
+    }).catchError((e) {
+      emit(CubitGetUserInfoFailed(e.toString()));
+    });
+  }
   Future<void> uploadProfileImage(
       {required File photo,
       required String userId,
