efe0f60ba4ab4d30e1195047f04c9e37fce84f60
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-13 05:00:30 +0200

    create CommentLikesCubit

---
+++ lib/presentation/cubit/postInfoCubit/commentsInfo/comment_likes/comment_likes_cubit.dart
@@ -0,0 +1,48 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instegram/domain/usecases/postUseCase/comments/put_like.dart';
+import 'package:instegram/domain/usecases/postUseCase/comments/remove_like.dart';
+
+part 'comment_likes_state.dart';
+
+class CommentLikesCubit extends Cubit<CommentLikesState> {
+  final PutLikeOnThisCommentUseCase _putLikeOnThisCommentUseCase;
+  final RemoveLikeOnThisCommentUseCase _removeLikeOnThisCommentUseCase;
+  CommentLikesCubit(
+      this._putLikeOnThisCommentUseCase, this._removeLikeOnThisCommentUseCase)
+      : super(CommentLikesInitial());
+
+  static CommentLikesCubit get(BuildContext context) =>
+      BlocProvider.of(context);
+
+  Future<void> putLikeOnThisComment(
+      {required String postId,
+      required String commentId,
+      required String myPersonalId}) async {
+    emit(CubitCommentLikesLoading());
+
+    await _putLikeOnThisCommentUseCase
+        .call(paramsOne: commentId, paramsTwo: myPersonalId)
+        .then((_) {
+      emit(CubitCommentLikesLoaded());
+    }).catchError((e) {
+      emit(CubitCommentLikesFailed(e.toString()));
+    });
+  }
+
+  Future<void> removeLikeOnThisComment(
+      {required String postId,
+        required String commentId,
+        required String myPersonalId}) async {
+    emit(CubitCommentLikesLoading());
+
+    await _removeLikeOnThisCommentUseCase
+        .call(paramsOne: commentId, paramsTwo: myPersonalId)
+        .then((_) {
+      emit(CubitCommentLikesLoaded());
+    }).catchError((e) {
+      emit(CubitCommentLikesFailed(e.toString()));
+    });
+  }
+}
