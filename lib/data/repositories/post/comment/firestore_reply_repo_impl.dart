a9fdfc4bae0c7d011e4b748398daa9038ead539f
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-13 04:48:06 +0200

    add removeLikeOnThisComment

--- lib/data/repositories/post/comment/firestore_reply_repo_impl.dart
+++ lib/data/repositories/post/comment/firestore_reply_repo_impl.dart
@@ -1,35 +1,23 @@
-import 'package:instegram/data/datasourses/remote/firestore_user_info.dart';
+import 'package:instegram/data/datasourses/remote/post/comment/firestore_comment.dart';
 import 'package:instegram/data/datasourses/remote/post/comment/firestore_reply.dart';
 import 'package:instegram/data/models/comment.dart';
 import 'package:instegram/data/models/reply_comment.dart';
-import 'package:instegram/data/models/user_personal_info.dart';
 import 'package:instegram/domain/repositories/post/comment/reply_repository.dart';
 
 class FirestoreRepliesRepositoryImpl implements FirestoreReplyRepository {
-  //TODO think on this
   @override
-  Future<Comment> replyOnThisComment(
-      {required String postId,
-      required String commentId,
-      required Comment replyInfo}) async {
+  Future<Comment> replyOnThisComment({required Comment replyInfo}) async {
     try {
-      String replyId = await FirestoreReply.replyOnThisComment(
-          postId: postId, commentId: commentId, replyInfo: replyInfo);
+      String replyId =
+          await FirestoreReply.replyOnThisComment(replyInfo: replyInfo);
 
-      Comment replyCommentInfo = await FirestoreReply.getReplyInfo(
-          postId: postId, commentId: commentId, replyId: replyId);
+      Comment theReplyInfo =
+          await FirestoreReply.getReplyInfo(replyId: replyId);
 
-      UserPersonalInfo whoReplyInfo =
-          await FirestoreUser.getUserInfo(replyCommentInfo.commentatorId);
-
-      UserPersonalInfo infoOfPersonIReplyOnThem =
-          await FirestoreUser.getUserInfo(
-              replyCommentInfo.idOfPersonIReplyOnThem!);
-
-      replyCommentInfo.commentatorInfo = whoReplyInfo;
-      replyCommentInfo.infoOfPersonIReplyOnThem = infoOfPersonIReplyOnThem;
-
-      return replyCommentInfo;
+      await FirestoreComment.putReplyOnThisComment(
+          commentId: theReplyInfo.parentCommentId,
+          replyId: theReplyInfo.commentUid);
+      return theReplyInfo;
     } catch (e) {
       return Future.error(e.toString());
     }
@@ -37,16 +25,10 @@ class FirestoreRepliesRepositoryImpl implements FirestoreReplyRepository {
 
   @override
   Future<void> putLikeOnThisReply(
-      {required String postId,
-      required String commentId,
-      required String replyId,
-      required String myPersonalId}) async {
+      {required String replyId, required String myPersonalId}) async {
     try {
       await FirestoreReply.putLikeOnThisReply(
-          postId: postId,
-          commentId: commentId,
-          replyId: replyId,
-          myPersonalId: myPersonalId);
+          replyId: replyId, myPersonalId: myPersonalId);
     } catch (e) {
       return Future.error(e.toString());
     }
@@ -54,31 +36,23 @@ class FirestoreRepliesRepositoryImpl implements FirestoreReplyRepository {
 
   @override
   Future<void> removeLikeOnThisReply(
-      {required String postId,
-      required String commentId,
-      required String replyId,
-      required String myPersonalId}) async {
+      {required String replyId, required String myPersonalId}) async {
     try {
       await FirestoreReply.removeLikeOnThisReply(
-          postId: postId,
-          commentId: commentId,
-          replyId: replyId,
-          myPersonalId: myPersonalId);
+          replyId: replyId, myPersonalId: myPersonalId);
     } catch (e) {
       return Future.error(e.toString());
     }
   }
 
   @override
-  Future<List<Comment>> getRepliesOfThisComment(
-      {required String postId, required String commentId}) async {
+  Future<List<Comment>> getSpecificReplies({required String commentId}) async {
     try {
-      List<Comment> theReply =
-          await FirestoreReply.getRepliesOfThisComment(
-              postId: postId, commentId: commentId);
-      List<Comment> reformatTheComments =
-          await FirestoreReply.getRepliesInfo(repliesInfo: theReply);
-      return reformatTheComments;
+      List? repliesIds =
+          await FirestoreComment.getRepliesOfComments(commentId: commentId);
+      List<Comment> theRepliesInfo =
+          await FirestoreReply.getSpecificReplies(repliesIds: repliesIds!);
+      return theRepliesInfo;
     } catch (e) {
       return Future.error(e.toString());
     }
