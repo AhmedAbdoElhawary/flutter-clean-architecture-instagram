1f7d6e65054e1bf6a06d4937605600b8a8c69562
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-13 04:59:08 +0200

    GetPostsInfoUseCase

---
+++ lib/domain/usecases/postUseCase/getPostInfo/get_post_info.dart
@@ -0,0 +1,14 @@
+import 'package:instegram/data/models/post.dart';
+import 'package:instegram/domain/repositories/post/post_repository.dart';
+import '../../../../core/usecase/usecase.dart';
+
+class GetPostsInfoUseCase implements UseCase<List<Post>, List<dynamic>> {
+  final FirestorePostRepository _getPostRepository;
+
+  GetPostsInfoUseCase(this._getPostRepository);
+
+  @override
+  Future<List<Post>> call({required List<dynamic> params}) {
+    return _getPostRepository.getPostsInfo(params);
+  }
+}
