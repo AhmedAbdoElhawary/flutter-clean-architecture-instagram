a9b9f9dbe1ef349fe63ed2d82ecc0db411c5340d
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-16 23:19:57 +0200

    reformating fromDocSnap

--- lib/data/models/user_personal_info.dart
+++ lib/data/models/user_personal_info.dart
@@ -22,8 +22,9 @@ class UserPersonalInfo {
       this.userName = "",
       this.userId = ""});
 
-  static UserPersonalInfo fromSnap(
-      DocumentSnapshot<Map<String, dynamic>> snap) {
+  static UserPersonalInfo fromDocSnap(
+      {DocumentSnapshot<Map<String, dynamic>>? docSnap,Map<String, dynamic>? mapSnap}) {
+    dynamic snap=docSnap ?? mapSnap;
     return UserPersonalInfo(
       name: snap["name"],
       userId: snap["uid"],
