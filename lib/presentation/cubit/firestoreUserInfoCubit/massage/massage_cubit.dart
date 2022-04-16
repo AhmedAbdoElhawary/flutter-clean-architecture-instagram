40361200ae13f20e194ff5daf3f6a2663d5834b9
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-16 23:17:26 +0200

    create MassageCubit

---
+++ lib/presentation/cubit/firestoreUserInfoCubit/massage/massage_cubit.dart
@@ -0,0 +1,39 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instegram/data/models/massage.dart';
+import 'package:instegram/domain/usecases/firestoreUserUseCase/massage/add_massage.dart';
+import 'package:instegram/domain/usecases/firestoreUserUseCase/massage/get_massages.dart';
+
+part 'massage_state.dart';
+
+class MassageCubit extends Cubit<MassageState> {
+  final AddMassageUseCase _addMassageUseCase;
+  final GetMassagesUseCase _getMassagesUseCase;
+  List<Massage> massagesInfo = [];
+  MassageCubit(this._addMassageUseCase, this._getMassagesUseCase)
+      : super(MassageInitial());
+
+  static MassageCubit get(BuildContext context) => BlocProvider.of(context);
+
+  Future<void> sendMassage(Massage massageInfo) async {
+    emit(SendMassageLoading());
+    await _addMassageUseCase.call(params: massageInfo).then((massageInfo) {
+      emit(SendMassageLoaded(massageInfo));
+    }).catchError((e) {
+      emit(SendMassageFailed(e.toString()));
+    });
+  }
+
+  Stream<void> getMassages(String receiverId) async* {
+    _getMassagesUseCase.call(params: receiverId).listen((event) {
+      massagesInfo = [];
+      for (var element in event.docs) {
+        Map<String, dynamic> data = element.data();
+        massagesInfo.add(Massage.fromJson(data));
+      }
+      emit(GetMassageSuccess());
+    });
+    // emit(GetMassageFailed(e.toString()));
+  }
+}
