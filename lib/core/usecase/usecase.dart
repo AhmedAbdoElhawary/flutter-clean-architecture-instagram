ec44aa7507425a6ae7a86e6bf1caf14b33d807f8
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-16 23:24:18 +0200

    create StreamUseCase

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -1,6 +1,9 @@
 abstract class UseCase<R, P> {
   Future<R> call({required P params});
 }
+abstract class StreamUseCase<R, P> {
+  Stream<R> call({required P params});
+}
 
 abstract class UseCaseTwoParams<R, A, B> {
   Future<R> call({required A paramsOne, required B paramsTwo});
