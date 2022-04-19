866eb0f3392819deb781c6693b1e9a74018755c3
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-19 23:50:43 +0200

    create IconsAssets

---
+++ lib/core/resources/assets_manager.dart
@@ -0,0 +1,21 @@
+const String iconPath = "assets/icons";
+
+class IconsAssets {
+  static const String sendIcon = "$iconPath/send.svg";
+  static const String menuIcon = "$iconPath/menu.svg";
+  static const String menuHorizontalIcon = "$iconPath/menu_horizontal.svg";
+  static const String commentIcon = "$iconPath/comment.svg";
+  static const String instagramLogo = "$iconPath/logo_instagram.svg";
+  static const String addIcon = "$iconPath/add.svg";
+  static const String minusIcon = "$iconPath/minus.svg";
+  static const String gridIcon = "$iconPath/grid.svg";
+  static const String videoIcon = "$iconPath/video.svg";
+  static const String cancelIcon = "$iconPath/cancel.svg";
+  static const String heartIcon = "$iconPath/heart.svg";
+
+  static const String addInstagramStoryIcon =
+      "$iconPath/add-instagram-story.svg";
+  static const String instagramHighlightStoryIcon =
+      "$iconPath/instagram-highlight-story.svg";
+
+}
