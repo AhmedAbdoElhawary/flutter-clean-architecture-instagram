ee4e8974aa56ef8febc967c4a9147471448b91bd
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-20 23:12:19 +0200

    create FontWeightManager,FontSize

---
+++ lib/core/resources/font_manager.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class FontConstants {
+  static const String fontFamily = "Style";
+}
+
+class FontWeightManager {
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.normal;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight bold = FontWeight.bold;
+}
+
+class FontSize {
+  static const double s0 = 0;
+  static const double s13 = 13.0;
+  static const double s14 = 14.0;
+  static const double s15 = 15.0;
+  static const double s16 = 16.0;
+  static const double s17 = 17.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+}
