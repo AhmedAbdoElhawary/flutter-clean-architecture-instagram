b5c0fb7d8a33e4e37cdf86399046a8afce02a04b
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-23 09:38:28 +0200

    change to UseCaseThreeParams

--- lib/domain/usecases/firestoreUserUseCase/massage/add_massage.dart
+++ lib/domain/usecases/firestoreUserUseCase/massage/add_massage.dart
@@ -2,13 +2,20 @@ import 'package:instegram/core/usecase/usecase.dart';
 import 'package:instegram/data/models/massage.dart';
 import 'package:instegram/domain/repositories/user_repository.dart';
 
-class AddMassageUseCase implements UseCaseTwoParams<Massage, Massage,String> {
+class AddMassageUseCase
+    implements UseCaseThreeParams<Massage, Massage, String, String> {
   final FirestoreUserRepository _addPostToUserRepository;
 
   AddMassageUseCase(this._addPostToUserRepository);
 
   @override
-  Future<Massage> call({required Massage paramsOne,required String paramsTwo,}) {
-    return _addPostToUserRepository.sendMassage(massageInfo: paramsOne,pathOfPhoto:paramsTwo );
+  Future<Massage> call(
+      {required Massage paramsOne,
+      required String paramsTwo,
+      required String paramsThree}) {
+    return _addPostToUserRepository.sendMassage(
+        massageInfo: paramsOne,
+        pathOfPhoto: paramsTwo,
+        pathOfRecorded: paramsThree);
   }
 }
