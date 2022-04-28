c0d52a8cb3498f7c4341f1e4c4f3924468481d0d
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-28 14:34:31 +0200

    add some colors in ColorManager

--- lib/core/resources/color_manager.dart
+++ lib/core/resources/color_manager.dart
@@ -2,14 +2,19 @@ import 'package:flutter/material.dart';
 
 class ColorManager {
   static const Color grey = Color(0xff9E9E9E);
-  static Color lightGrey = const Color(0x2d868585);
+  static const Color lightGrey = Color(0xEEEEEEFF);
+  static const Color lowOpacityGrey = Color(0x3B3B3B12);
   static const Color black = Color(0xff000000);
-  static const Color black87 = Color(0xdd000000);
-  static const Color black54 = Color(0x8a000000);
+  static const Color black12 = Color(0x0000001F);
   static const Color black26 = Color(0x42000000);
+  static const Color black38 = Colors.black38;
+  static const Color black54 = Color(0x8a000000);
+  static const Color black87 = Color(0xdd000000);
   static const Color teal = Color(0xFF009688);
   static const Color blue = Color(0xFF2196F3);
+  static const Color darkBlue = Color.fromARGB(255, 4, 113, 238);
   static const Color lightBlue = Color(0xFF93C6F6);
   static const Color white = Color(0xffFFFFFF);
-  static const Color error = Color(0xffe61f34);
+  static const Color redAccent = Colors.redAccent;
+  static const Color red = Color(0xffe61f34);
 }
