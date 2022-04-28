000b2959d9d0723c996f50f1f28c7f2850a858ae
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-28 14:34:09 +0200

    create AppTheme

--- lib/config/themes/app_theme.dart
+++ lib/config/themes/app_theme.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:instegram/core/resources/color_manager.dart';
+import 'package:instegram/core/resources/font_manager.dart';
+import 'package:instegram/core/resources/styles_manager.dart';
 
 class AppTheme {
   static ThemeData get light {
     return ThemeData(
-      appBarTheme: const AppBarTheme(
-        elevation: 0,
-        color: Colors.white,
-      ),
       primarySwatch: Colors.grey,
       scaffoldBackgroundColor: Colors.white,
       canvasColor: Colors.transparent,
+      appBarTheme: AppBarTheme(
+        elevation: 0,
+        color: ColorManager.white,
+        shadowColor: ColorManager.lowOpacityGrey,
+        titleTextStyle:
+        getNormalStyle(fontSize: FontSize.s16),
+      ),
     );
   }
 }
