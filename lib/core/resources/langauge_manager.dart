de4881f8b2f34cbcfdf9f85dfeb19404c48418f7
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-04-30 21:29:09 +0200

    create LanguageTypeExtension

---
+++ lib/core/resources/langauge_manager.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+ enum  LanguageType{ english, arabic }
+
+const String arabic = "ar";
+const String english = "en";
+const String assetPathLocalisations = "assets/translations";
+
+const Locale arabicLocal = Locale("ar", "SA");
+const Locale englishLocal = Locale("en", "US");
+
+extension LanguageTypeExtension on LanguageType {
+  String getValue() {
+    switch (this) {
+      case LanguageType.english:
+        return english;
+      case LanguageType.arabic:
+        return arabic;
+    }
+  }
+}
