1c55b47074715d297b58ba741b85087aefb5e023
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-05-06 23:42:28 +0200

    change the values of colors

--- lib/core/resources/color_manager.dart
+++ lib/core/resources/color_manager.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 
 class ColorManager {
-  static const Color grey = Color(0xff9E9E9E);
+  static const Color white = Color(0xffFFFFFF);
   static const Color lightGrey = Color(0xEEEEEEFF);
+  static const Color grey = Color(0xff9E9E9E);
   static const Color lowOpacityGrey = Color(0x2A3E3E2F);
   static const Color veryLowOpacityGrey = Color(0x16444439);
 
   static const Color black = Color(0xff000000);
-  static const Color black12 = Color(0x0000001F);
-  static const Color black26 = Color(0x42000000);
-  static const Color black38 = Colors.black38;
-  static const Color black54 = Color(0x8a000000);
   static const Color black87 = Color(0xdd000000);
+  static const Color black54 = Color(0x8a000000);
+  static const Color black26 = Color(0x42000000);
+  static const Color black12 = Color(0x0000001F);
+
+  static const Color black45 = Colors.black45;
+  static const Color black38 = Colors.black38;
+
   static const Color teal = Color(0xFF009688);
   static const Color blue = Color(0xFF2196F3);
   static const Color darkBlue = Color.fromARGB(255, 4, 113, 238);
-  static const Color lightBlue = Color(0xFF93C6F6);
-  static const Color white = Color(0xffFFFFFF);
+  static const Color lightBlue = Color(0xFF7FC1FF);
   static const Color redAccent = Colors.redAccent;
   static const Color red = Color(0xffe61f34);
   static const Color blackRed = Color.fromARGB(255, 182, 14, 14);
   static const Color yellow = Color.fromARGB(255, 243, 252, 3);
+
+  static const Color transparent = Colors.transparent;
 }
