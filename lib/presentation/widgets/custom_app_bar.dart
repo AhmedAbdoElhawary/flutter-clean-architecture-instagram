a7e71ab1cf941e92e7346531588bd9b271fb766d
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-05-07 23:20:21 +0200

    add oneTitleAppBar and others

--- lib/presentation/widgets/custom_app_bar.dart
+++ lib/presentation/widgets/custom_app_bar.dart
@@ -1,42 +1,81 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instegram/core/resources/assets_manager.dart';
+import 'package:instegram/core/resources/styles_manager.dart';
 
-AppBar customAppBar(BuildContext context) {
-  return AppBar(
-    backgroundColor: Theme.of(context).primaryColor,
-    centerTitle: false,
-    iconTheme: IconThemeData(color: Theme.of(context).focusColor),
-    title: SvgPicture.asset(
-      IconsAssets.instagramLogo,
-      height: 32,
-      color: Theme.of(context).focusColor,
-    ),
-    actions: [
-      IconButton(
-        icon: SvgPicture.asset(
-          IconsAssets.addIcon,
-          color: Theme.of(context).focusColor,
-          height: 22.5,
-        ),
-        onPressed: () {},
+class CustomAppBar {
+  static AppBar basicAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).primaryColor,
+      centerTitle: false,
+      iconTheme: IconThemeData(color: Theme.of(context).focusColor),
+      title: SvgPicture.asset(
+        IconsAssets.instagramLogo,
+        height: 32,
+        color: Theme.of(context).focusColor,
       ),
-      IconButton(
-        icon: SvgPicture.asset(
-          IconsAssets.heartIcon,
-          color: Theme.of(context).focusColor,
-          height: 22.5,
+      actions: [
+        IconButton(
+          icon: SvgPicture.asset(
+            IconsAssets.addIcon,
+            color: Theme.of(context).focusColor,
+            height: 22.5,
+          ),
+          onPressed: () {},
         ),
-        onPressed: () {},
+        IconButton(
+          icon: SvgPicture.asset(
+            IconsAssets.heartIcon,
+            color: Theme.of(context).focusColor,
+            height: 22.5,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: SvgPicture.asset(
+            IconsAssets.send1Icon,
+            color: Theme.of(context).focusColor,
+            height: 22.5,
+          ),
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+
+  static AppBar oneTitleAppBar(BuildContext context, String text) {
+    return AppBar(
+      backgroundColor: Theme.of(context).primaryColor,
+      centerTitle: false,
+      iconTheme: IconThemeData(color: Theme.of(context).focusColor),
+      title: Text(
+        text,
+        style:
+            getMediumStyle(color: Theme.of(context).focusColor, fontSize: 20),
       ),
-      IconButton(
-        icon: SvgPicture.asset(
-          IconsAssets.send1Icon,
-          color: Theme.of(context).focusColor,
-          height: 22.5,
-        ),
-        onPressed: () {},
-      )
-    ],
-  );
+    );
+  }
+
+  static AppBar menuOfUserAppBar(
+      BuildContext context, String text, AsyncCallback bottomSheet) {
+    return AppBar(
+        elevation: 0,
+        iconTheme: IconThemeData(color: Theme.of(context).focusColor),
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text(text,
+            style: getMediumStyle(
+                color: Theme.of(context).focusColor, fontSize: 20)),
+        actions: [
+          IconButton(
+            icon: SvgPicture.asset(
+              IconsAssets.menuHorizontalIcon,
+              color: Theme.of(context).focusColor,
+              height: 22.5,
+            ),
+            onPressed: () => bottomSheet,
+          ),
+          const SizedBox(width: 5)
+        ]);
+  }
 }
