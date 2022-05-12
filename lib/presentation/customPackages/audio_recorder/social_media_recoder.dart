04539d25570447aaf61654c146f19c0d303936ae
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-05-13 00:13:32 +0200

    create SocialMediaRecorder widget

---
+++ lib/presentation/customPackages/audio_recorder/social_media_recoder.dart
@@ -0,0 +1,151 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:instagram/core/resources/color_manager.dart';
+import 'package:instagram/presentation/customPackages/audio_recorder/lock_record.dart';
+import 'package:instagram/presentation/customPackages/audio_recorder/show_counter.dart';
+import 'package:instagram/presentation/customPackages/audio_recorder/show_mic_and_counter.dart';
+import 'package:instagram/presentation/customPackages/audio_recorder/sound_recorder_notifier.dart';
+import 'package:instagram/presentation/customPackages/audio_recorder/sound_recorder_when_locked_design.dart';
+import 'package:provider/provider.dart';
+
+class SocialMediaRecorder extends StatefulWidget {
+  /// function reture the recording sound file
+  final Function(File soundFile) sendRequestFunction;
+
+  /// text to know user should drag in the left to cancel record
+  final String? slideToCancelText;
+
+  /// this text show when lock record and to tell user should press in this text to cancel recod
+  final String? cancelText;
+  final ValueChanged<bool> showIcons;
+
+  // ignore: sort_constructors_first
+  const SocialMediaRecorder({
+    required this.sendRequestFunction,
+    required this.showIcons,
+    this.slideToCancelText = " Slide to Cancel >",
+    this.cancelText = "Cancel",
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _SocialMediaRecorder createState() => _SocialMediaRecorder();
+}
+
+class _SocialMediaRecorder extends State<SocialMediaRecorder> {
+  late SoundRecordNotifier soundRecordNotifier;
+
+  @override
+  void initState() {
+    soundRecordNotifier = SoundRecordNotifier();
+    soundRecordNotifier.initialStorePathRecord = "";
+    soundRecordNotifier.isShow = false;
+    soundRecordNotifier.voidInitialSound();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => soundRecordNotifier),
+        ],
+        child: Consumer<SoundRecordNotifier>(
+          builder: (context, value, _) {
+            return Directionality(
+                textDirection: TextDirection.rtl, child: makeBody(value));
+          },
+        ));
+  }
+
+  Widget makeBody(SoundRecordNotifier state) {
+    return GestureDetector(
+      onHorizontalDragUpdate: (scrollEnd) {
+        state.updateScrollValue(scrollEnd.globalPosition, context);
+      },
+      onHorizontalDragEnd: (x) {},
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(12),
+            topRight: Radius.circular(12),
+          ),
+        ),
+        child: recordVoice(state),
+      ),
+    );
+  }
+
+  Widget recordVoice(SoundRecordNotifier state) {
+    if (state.lockScreenRecord == true) {
+      return SoundRecorderWhenLockedDesign(
+        cancelText: widget.cancelText,
+        showIcons: widget.showIcons,
+        recordIconWhenLockBackGroundColor: Colors.blue,
+        sendRequestFunction: widget.sendRequestFunction,
+        soundRecordNotifier: state,
+      );
+    }
+
+    return Listener(
+      onPointerDown: (details) async {
+        state.setNewInitialDraggableHeight(details.position.dy);
+        state.resetEdgePadding();
+        widget.showIcons(false);
+        soundRecordNotifier.isShow = true;
+        state.record();
+      },
+      onPointerUp: (details) async {
+        if (!state.isLocked) {
+          if (state.buttonPressed) {
+            if (state.second > 1 || state.minute > 0) {
+              String path = state.mPath;
+              widget.sendRequestFunction(File.fromUri(Uri(path: path)));
+            }
+            widget.showIcons(true);
+          }
+          state.resetEdgePadding();
+        }
+      },
+      child: AnimatedContainer(
+          duration:
+              Duration(milliseconds: soundRecordNotifier.isShow ? 0 : 300),
+          height: 50,
+          width: (soundRecordNotifier.isShow)
+              ? MediaQuery.of(context).size.width * .8
+              : 40,
+          child: Stack(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right: state.edge),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: soundRecordNotifier.isShow
+                        ? BorderRadius.circular(12)
+                        : BorderRadius.circular(0),
+                    color: ColorManager.transparent,
+                  ),
+                  child: Stack(
+                    children: [
+                      ShowMicWithText(
+                        shouldShowText: soundRecordNotifier.isShow,
+                        soundRecorderState: state,
+                        slideToCancelText: widget.slideToCancelText,
+                      ),
+                      if (soundRecordNotifier.isShow)
+                        ShowCounter(soundRecorderState: state),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(width: 60, child: LockRecord(soundRecorderState: state))
+            ],
+          )),
+    );
+  }
+}
