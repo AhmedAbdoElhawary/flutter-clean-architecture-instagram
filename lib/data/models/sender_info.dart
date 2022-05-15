50a6def44323e22f9a0dc5e7a602b1b6fad17977
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-05-15 23:48:08 +0200

    create SenderInfo

---
+++ lib/data/models/sender_info.dart
@@ -0,0 +1,13 @@
+import 'package:instagram/data/models/message.dart';
+import 'package:instagram/data/models/user_personal_info.dart';
+
+/// it can be sender or receiver
+class SenderInfo {
+  UserPersonalInfo? userInfo;
+  Message lastMessage;
+
+  SenderInfo({
+    this.userInfo,
+    required this.lastMessage,
+  });
+}
