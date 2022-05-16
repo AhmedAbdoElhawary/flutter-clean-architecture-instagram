2292672ccefef2a5b98acfc1b4406212fbac748e
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-05-16 23:10:01 +0200

    sperate image and video in two lists

--- lib/presentation/widgets/belong_to/time_line_w/all_time_line_grid_view.dart
+++ lib/presentation/widgets/belong_to/time_line_w/all_time_line_grid_view.dart
@@ -1,30 +1,31 @@
-import 'dart:ui';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instagram/core/resources/strings_manager.dart';
 import 'package:instagram/data/models/post.dart';
-import 'package:instagram/presentation/pages/video/play_this_video.dart';
-import 'package:instagram/presentation/widgets/belong_to/time_line_w/animated_dialog.dart';
-import 'package:instagram/presentation/widgets/global/circle_avatar_image/circle_avatar_of_profile_image.dart';
-import 'package:instagram/presentation/widgets/global/custom_widgets/custom_image_display.dart';
-import 'package:instagram/presentation/widgets/global/custom_widgets/custom_posts_display.dart';
+import 'package:instagram/presentation/customPackages/in_view_notifier/in_view_notifier_custom.dart';
+import 'package:instagram/presentation/customPackages/in_view_notifier/in_view_notifier_widget.dart';
+import 'package:instagram/presentation/widgets/global/custom_widgets/custom_grid_view_display.dart';
 
 // ignore: must_be_immutable
 class AllTimeLineGridView extends StatefulWidget {
   List<Post> postsImagesInfo;
   List<Post> postsVideosInfo;
   List<Post> allPostsInfo;
-  ValueNotifier<bool> isThatEndOfList;
   final bool isThatProfile;
+  final ValueNotifier<bool> isThatEndOfList;
+  final AsyncValueSetter<int> onRefreshData;
+  final ValueNotifier<bool> reloadData;
 
   AllTimeLineGridView(
       {required this.postsImagesInfo,
       required this.postsVideosInfo,
-        required this.isThatEndOfList,
-        required this.allPostsInfo,
+      required this.isThatEndOfList,
+      required this.allPostsInfo,
+      required this.onRefreshData,
       this.isThatProfile = true,
+      required this.reloadData,
       Key? key})
       : super(key: key);
 
@@ -33,170 +34,107 @@ class AllTimeLineGridView extends StatefulWidget {
 }
 
 class _CustomGridViewState extends State<AllTimeLineGridView> {
-  OverlayEntry? _popupDialog;
   int indexOfPostsVideo = 0;
   int indexOfPostsImage = 0;
+  @override
+  void didUpdateWidget(covariant AllTimeLineGridView oldWidget) {
+    if (oldWidget.reloadData.value) {
+      indexOfPostsVideo = 0;
+      indexOfPostsImage = 0;
+      oldWidget.reloadData.value = false;
+    }
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return widget.postsImagesInfo.isNotEmpty
-        ? StaggeredGridView.countBuilder(
-            padding: const EdgeInsetsDirectional.only(bottom: 1.5, top: 1.5),
-            crossAxisSpacing: 1.5,
-            mainAxisSpacing: 1.5,
-            crossAxisCount: 3,
-            primary: false,
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            itemCount:
-                widget.postsImagesInfo.length + widget.postsVideosInfo.length,
-            itemBuilder: (context, index) {
-              Post postInfo;
-              if (index == 2 || (index % 11 == 0 && index != 0)) {
+    return InViewNotifierCustomScrollView(
+      slivers: [
+        SliverStaggeredGrid.countBuilder(
+          crossAxisSpacing: 1.5,
+          mainAxisSpacing: 1.5,
+          crossAxisCount: 3,
+          itemCount:
+              widget.postsImagesInfo.length + widget.postsVideosInfo.length,
+          itemBuilder: (context, index) {
+            Post postInfo;
+            if (indexOfPostsVideo >= widget.postsVideosInfo.length &&
+                indexOfPostsImage < widget.postsImagesInfo.length) {
+              postInfo = widget.postsImagesInfo[indexOfPostsImage];
+              indexOfPostsImage++;
+            } else if (indexOfPostsVideo < widget.postsVideosInfo.length &&
+                indexOfPostsImage >= widget.postsImagesInfo.length) {
+              postInfo = widget.postsVideosInfo[indexOfPostsVideo];
+              indexOfPostsVideo++;
+            } else {
+              if (indexOfPostsVideo >= widget.postsVideosInfo.length &&
+                  indexOfPostsImage >= widget.postsImagesInfo.length) {
+                indexOfPostsVideo = 0;
+                indexOfPostsImage = 0;
+              }
+              if ((index == 2 || (index % 11 == 0 && index != 0)) &&
+                  indexOfPostsVideo < widget.postsVideosInfo.length) {
                 postInfo = widget.postsVideosInfo[indexOfPostsVideo];
                 indexOfPostsVideo++;
               } else {
                 postInfo = widget.postsImagesInfo[indexOfPostsImage];
                 indexOfPostsImage++;
               }
-              if(index==widget.allPostsInfo.length-1){
-                widget.isThatEndOfList.value=true;
-              }
-              return createGridTileWidget(postInfo, widget.allPostsInfo, index);
-            },
-            staggeredTileBuilder: (index) {
-              double num = (index == 2 || (index % 11 == 0 && index != 0)) ? 2 : 1;
-              return StaggeredTile.count(1, num);
-            },
-          )
-        : Center(
-            child: Text(
-            StringsManager.noPosts.tr(),
-            style: Theme.of(context).textTheme.bodyText1,
-          ));
-  }
-
-  Widget createGridTileWidget(
-          Post postClickedInfo, List<Post> postsInfo, int index) =>
-      Builder(
-        builder: (context) => GestureDetector(
-          onTap: () {
-            List<Post> customPostsInfo = postsInfo;
-            customPostsInfo.removeWhere(
-                (value) => value.postUid == postClickedInfo.postUid);
-            customPostsInfo.insert(0, postClickedInfo);
-            Navigator.of(context).push(CupertinoPageRoute(
-              builder: (context) => CustomPostsDisplay(
-                postsInfo: postsInfo,
-                isThatProfile: widget.isThatProfile,
-              ),
-            ));
+            }
+            if (index == widget.allPostsInfo.length - 1) {
+              widget.isThatEndOfList.value = true;
+            }
+            return inViewWidget(index, postInfo);
           },
-          onLongPress: () {
-            _popupDialog = _createPopupDialog(postClickedInfo);
-            Overlay.of(context)!.insert(_popupDialog!);
+          staggeredTileBuilder: (index) {
+            double num =
+                (index == 2 || (index % 11 == 0 && index != 0)) ? 2 : 1;
+            return StaggeredTile.count(1, num);
           },
-          onLongPressEnd: (details) => _popupDialog?.remove(),
-          child: postClickedInfo.isThatImage
-              ? ImageDisplay(
-                  imageUrl: postClickedInfo.postUrl, boxFit: BoxFit.cover)
-              : PlayThisVideo(videoUrl: postClickedInfo.postUrl, play: false),
         ),
-      );
-
-  OverlayEntry _createPopupDialog(Post postInfo) {
-    return OverlayEntry(
-      builder: (context) => BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 25, sigmaY: 20),
-        child: AnimatedDialog(
-          child: _createPopupContent(postInfo),
-        ),
-      ),
+      ],
+      onRefreshData: widget.onRefreshData,
+      postsIds: widget.allPostsInfo,
+      isThatEndOfList: widget.isThatEndOfList,
+      initialInViewIds: const ['0'],
+      isInViewPortCondition:
+          (double deltaTop, double deltaBottom, double viewPortDimension) {
+        return deltaTop < (0.5 * viewPortDimension) &&
+            deltaBottom > (0.7 * viewPortDimension);
+      },
     );
   }
 
-  Widget _createPopupContent(Post postInfo) {
-    double bodyHeight = MediaQuery.of(context).size.height;
-
+  Container inViewWidget(int index, Post postInfo) {
     return Container(
-      padding: const EdgeInsetsDirectional.only(start: 10, end: 10),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            _createPhotoTitle(postInfo),
-            postInfo.isThatImage
-                ? Container(
-                    color: Theme.of(context).primaryColor,
-                    width: double.infinity,
-                    child: ImageDisplay(
-                        imageUrl: postInfo.postUrl, boxFit: BoxFit.fitWidth))
-                : Container(
-                    color: Theme.of(context).primaryColor,
-                    width: double.infinity,
-                    height: bodyHeight - 200,
-                    child:
-                        PlayThisVideo(videoUrl: postInfo.postUrl, play: true)),
-            _createActionBar(),
-          ],
-        ),
+      width: double.infinity,
+      margin: const EdgeInsetsDirectional.only(bottom: .5, top: .5),
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return InViewNotifierWidget(
+            id: '$index',
+            builder: (_, bool isInView, __) {
+              return CustomGridViewDisplay(
+                postClickedInfo: postInfo,
+                postsInfo: widget.allPostsInfo,
+                index: index,
+                playThisVideo: isInView,
+                isThatProfile: widget.isThatProfile,
+              );
+              // return columnOfWidgets(bodyHeight, index, isInView);
+            },
+          );
+        },
       ),
     );
   }
 
-  Widget _createPhotoTitle(Post postInfo) => Container(
-        padding: const EdgeInsetsDirectional.only(
-            bottom: 5, top: 5, end: 10, start: 10),
-        height: 55,
-        width: double.infinity,
-        color: Theme.of(context).splashColor,
-        child: Row(
-          children: [
-            CircleAvatarOfProfileImage(
-              userInfo: postInfo.publisherInfo!,
-              bodyHeight: 370,
-            ),
-            const SizedBox(width: 7),
-            Text(postInfo.publisherInfo!.name,
-                style: Theme.of(context).textTheme.bodyText1),
-          ],
-        ),
-      );
-
-  Widget _createActionBar() => Container(
-        height: 50,
-        padding: const EdgeInsetsDirectional.only(bottom: 5, top: 5),
-        color: Theme.of(context).splashColor,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            GestureDetector(
-              onPanStart: (d) {},
-              onForcePressStart: (e) {
-                e.pressure;
-              },
-              onLongPressUp: () {},
-              child: Icon(
-                Icons.favorite_border,
-                color: Theme.of(context).focusColor,
-              ),
-            ),
-            GestureDetector(
-              onVerticalDragStart: (d) {},
-              child: Icon(
-                Icons.chat_bubble_outline,
-                color: Theme.of(context).focusColor,
-              ),
-            ),
-            GestureDetector(
-              onTertiaryLongPress: () {},
-              child: Icon(
-                Icons.send,
-                color: Theme.of(context).focusColor,
-              ),
-            ),
-          ],
-        ),
-      );
+  Center noData() {
+    return Center(
+      child: Text(
+        StringsManager.noPosts.tr(),
+        style: Theme.of(context).textTheme.bodyText1,
+      ),
+    );
+  }
 }
