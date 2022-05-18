c52a13abe14b0e670d23ab6262f3889da436ffe6
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-05-19 00:07:45 +0200

    add compressVideo

--- lib/core/functions/compress_image.dart
+++ lib/core/functions/compress_image.dart
@@ -2,24 +2,32 @@ import 'dart:io';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:video_compress/video_compress.dart';
 
-Future<File?> compressFile(File file, {bool isThatImage = true}) async {
+Future<File?> compressImage(File file) async {
   final filePath = file.absolute.path;
 
   final lastIndex = filePath.lastIndexOf(RegExp(r'.jp'));
   final split = filePath.substring(0, (lastIndex));
   final outPath = "${split}_out${filePath.substring(lastIndex)}";
   if (file.lengthSync() > 200000) {
-    File? result = isThatImage
-        ? await FlutterImageCompress.compressAndGetFile(
-            file.absolute.path,
-            outPath,
-            quality: file.lengthSync() > 5000000 ? 70 : 50,
-          )
-        : await VideoCompress.getFileThumbnail(
-            file.absolute.path,
-            quality: file.lengthSync() > 5000000 ? 70 : 50,
-          );
+    File? result = await FlutterImageCompress.compressAndGetFile(
+      file.absolute.path,
+      outPath,
+      quality: file.lengthSync() > 5000000 ? 70 : 50,
+    );
     return result;
+  } else {
+    return file;
+  }
+}
+
+Future<File?> compressVideo(File file) async {
+  if (file.lengthSync() > 500000) {
+    MediaInfo? result = await VideoCompress.compressVideo(
+      file.path,
+      quality: VideoQuality.MediumQuality,
+      includeAudio: true,
+    );
+    return result!.file;
   } else {
     return file;
   }
