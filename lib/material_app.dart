7c6fe9685c3a9b6cff55dbd4e87a8c7870971c7f
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-06-03 04:20:32 +0200

    change the catching of theme

--- lib/material_app.dart
+++ lib/material_app.dart
@@ -1,9 +1,9 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:instagram/config/themes/app_theme.dart';
-import 'package:instagram/core/app_prefs.dart';
-import 'package:instagram/core/utility/injector.dart';
+import 'package:instagram/config/themes/theme_service.dart';
 import 'package:instagram/presentation/pages/register/login_page.dart';
 import 'package:instagram/presentation/widgets/belong_to/register_w/get_my_user_info.dart';
 import 'package:instagram/presentation/widgets/multi_bloc_provider.dart';
@@ -20,8 +20,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String? myId;
-  final AppPrefMode _appPreferences = injector<AppPrefMode>();
-  ThemeData? themeOfApp;
   final navigatorKey = GlobalKey<NavigatorState>();
   @override
   void initState() {
@@ -30,26 +28,21 @@ class _MyAppState extends State<MyApp> {
   }
 
   @override
-  void didChangeDependencies() {
-    themeOfApp = _appPreferences.getAppMode();
-    super.didChangeDependencies();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return MultiBlocs(materialApp(context));
   }
 
   Widget materialApp(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       navigatorKey: navigatorKey,
       locale: context.locale,
       supportedLocales: context.supportedLocales,
       localizationsDelegates: context.localizationDelegates,
       debugShowCheckedModeBanner: false,
       title: 'instagram',
-      theme: themeOfApp ?? AppTheme.light,
+      theme: AppTheme.light,
       darkTheme: AppTheme.dark,
+      themeMode: ThemeService().theme,
       home: AnimatedSplashScreen(
         centered: true,
         splash: Lottie.asset('assets/splash_gif/instagram.json'),
