f136fc38e7c25382fbfaa8de0b5e065fcf32f909
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-06-03 04:24:51 +0200

    create blurHashEncode

---
+++ lib/core/functions/blur_hash.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:blurhash/blurhash.dart';
+
+Future<String> blurHashEncode(File file) async {
+  Uint8List pixels = file.readAsBytesSync();
+  String result = await BlurHash.encode(pixels, 4, 3);
+  return result;
+}
