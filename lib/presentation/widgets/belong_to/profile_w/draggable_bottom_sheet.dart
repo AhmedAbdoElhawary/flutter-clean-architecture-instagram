c3c2a6181f58db77e0ba194b2ad24a97519159db
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-07-05 19:14:11 +0200

    create DraggableBottomSheet

---
+++ lib/presentation/widgets/belong_to/profile_w/draggable_bottom_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DraggableBottomSheet extends StatelessWidget {
+  const DraggableBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
