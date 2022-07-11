41b5056000551cbd138a26b5f2844bc7bdf9ec12
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-07-11 19:52:26 +0200

    make the icons more rounded

--- lib/presentation/widgets/belong_to/profile_w/show_me_the_users.dart
+++ lib/presentation/widgets/belong_to/profile_w/show_me_the_users.dart
@@ -5,7 +5,8 @@ import 'package:instagram/core/resources/color_manager.dart';
 import 'package:instagram/core/resources/strings_manager.dart';
 import 'package:instagram/data/models/user_personal_info.dart';
 import 'package:instagram/core/functions/toast_show.dart';
-import '../../../cubit/followCubit/follow_cubit.dart';
+import 'package:instagram/presentation/cubit/firestoreUserInfoCubit/user_info_cubit.dart';
+import '../../../cubit/follow/follow_cubit.dart';
 import '../../global/circle_avatar_image/circle_avatar_of_profile_image.dart';
 import 'package:instagram/core/utility/constant.dart';
 import 'package:instagram/presentation/widgets/belong_to/profile_w/which_profile_page.dart';
@@ -14,66 +15,72 @@ class ShowMeTheUsers extends StatefulWidget {
   final List<UserPersonalInfo> usersInfo;
   final bool isThatFollower;
   final ValueNotifier<UserPersonalInfo>? userInfo;
-  final ValueChanged<bool> rebuild;
   final bool showSearchBar;
+  final bool showColorfulCircle;
 
-  const ShowMeTheUsers(
-      {Key? key,
-      required this.usersInfo,
-      required this.isThatFollower,
-      required this.showSearchBar,
-      this.userInfo,
-      required this.rebuild})
-      : super(key: key);
+  const ShowMeTheUsers({
+    Key? key,
+    required this.usersInfo,
+    this.showColorfulCircle = true,
+    this.isThatFollower = true,
+    this.showSearchBar = true,
+    this.userInfo,
+  }) : super(key: key);
 
   @override
   State<ShowMeTheUsers> createState() => _ShowMeTheUsersState();
 }
 
 class _ShowMeTheUsersState extends State<ShowMeTheUsers> {
+  late UserPersonalInfo myPersonalInfo;
+  @override
+  initState() {
+    myPersonalInfo = FirestoreUserInfoCubit.getMyPersonalInfo(context);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          widget.usersInfo.isNotEmpty
-              ? ListView.builder(
-                  shrinkWrap: true,
-                  primary: false,
-                  addAutomaticKeepAlives: false,
-                  addRepaintBoundaries: false,
-                  itemBuilder: (context, index) {
-                    return containerOfUserInfo(
-                        widget.usersInfo[index], widget.isThatFollower);
-                  },
-                  itemCount: widget.usersInfo.length)
-              : Center(
-                  child: Text(
-                  widget.isThatFollower
-                      ? StringsManager.noFollowers.tr()
-                      : StringsManager.noFollowings.tr(),
-                  style: Theme.of(context).textTheme.bodyText1,
-                )),
-        ],
-      ),
-    );
+    if (widget.usersInfo.isNotEmpty) {
+      return SingleChildScrollView(
+        child: ListView.separated(
+            shrinkWrap: true,
+            primary: false,
+            addAutomaticKeepAlives: false,
+            addRepaintBoundaries: false,
+            itemBuilder: (context, index) {
+              return containerOfUserInfo(
+                  widget.usersInfo[index], widget.isThatFollower);
+            },
+          separatorBuilder: (context,index)=>const SizedBox(height: 10),
+            itemCount: widget.usersInfo.length,),
+      );
+    } else {
+      return Center(
+        child: Text(
+          widget.isThatFollower
+              ? StringsManager.noFollowers.tr()
+              : StringsManager.noFollowings.tr(),
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+      );
+    }
   }
 
   Widget containerOfUserInfo(UserPersonalInfo userInfo, bool isThatFollower) {
     String hash = "${userInfo.userId.hashCode}userInfo";
     return InkWell(
       onTap: () async {
-        await Navigator.of(context).push(MaterialPageRoute(
+        await Navigator.of(context).push(
+          MaterialPageRoute(
             builder: (context) => WhichProfilePage(
-                  userId: userInfo.userId,
-                )));
-
-        widget.rebuild(true);
+              userId: userInfo.userId,
+            ),
+          ),
+        );
       },
       child: Padding(
-        padding: const EdgeInsetsDirectional.only(end: 10, start: 10, top: 10),
+        padding: const EdgeInsetsDirectional.only( start: 10, top: 10),
         child: Row(children: [
           Hero(
             tag: hash,
@@ -81,6 +88,7 @@ class _ShowMeTheUsersState extends State<ShowMeTheUsers> {
               bodyHeight: 600,
               hashTag: hash,
               userInfo: userInfo,
+              showColorfulCircle:widget.showColorfulCircle ,
             ),
           ),
           const SizedBox(width: 10),
@@ -117,27 +125,20 @@ class _ShowMeTheUsersState extends State<ShowMeTheUsers> {
             } else {
               return GestureDetector(
                   onTap: () async {
-                    widget.rebuild(false);
-                    if (userInfo.followerPeople.contains(myPersonalId)) {
+                    if (myPersonalInfo.followedPeople
+                        .contains(userInfo.userId)) {
                       BlocProvider.of<FollowCubit>(followContext)
                           .removeThisFollower(
                               followingUserId: userInfo.userId,
                               myPersonalId: myPersonalId);
-                      if (widget.userInfo != null &&
-                          myPersonalId == userInfo.userId) {
-                        if (isThatFollower) {
-                          userInfo.followerPeople.remove(userInfo.userId);
-                        } else {
-                          userInfo.followedPeople.remove(userInfo.userId);
-                        }
-                      }
+                      myPersonalInfo.followedPeople.remove(userInfo.userId);
                     } else {
                       BlocProvider.of<FollowCubit>(followContext)
                           .followThisUser(
                               followingUserId: userInfo.userId,
                               myPersonalId: myPersonalId);
+                      myPersonalInfo.followedPeople.add(userInfo.userId);
                     }
-                    widget.rebuild(true);
                   },
                   child: whichContainerOfText(stateOfFollow, userInfo));
             }
@@ -152,7 +153,8 @@ class _ShowMeTheUsersState extends State<ShowMeTheUsers> {
     if (stateOfFollow is CubitFollowThisUserFailed) {
       ToastShow.toastStateError(stateOfFollow);
     }
-    return !userInfo.followerPeople.contains(myPersonalId)
+
+    return !myPersonalInfo.followedPeople.contains(userInfo.userId)
         ? containerOfFollowText(
             text: StringsManager.follow.tr(),
             isThatFollower: false,
@@ -163,27 +165,30 @@ class _ShowMeTheUsersState extends State<ShowMeTheUsers> {
           );
   }
 
-  Container containerOfFollowText(
+  Widget containerOfFollowText(
       {required String text, required bool isThatFollower}) {
-    return Container(
-      height: 35.0,
-      decoration: BoxDecoration(
-        color:
-            isThatFollower ? Theme.of(context).primaryColor : ColorManager.blue,
-        border: Border.all(
-            color: Theme.of(context).bottomAppBarColor,
-            width: isThatFollower ? 1.0 : 0),
-        borderRadius: BorderRadius.circular(6.0),
-      ),
-      child: Center(
-        child: Text(
-          text,
-          style: TextStyle(
-              fontSize: 17.0,
-              color: isThatFollower
-                  ? Theme.of(context).focusColor
-                  : ColorManager.white,
-              fontWeight: FontWeight.w500),
+    return Padding(
+      padding: const EdgeInsetsDirectional.only(start: 45,end: 15),
+      child: Container(
+        height: 32.0,
+        decoration: BoxDecoration(
+          color:
+              isThatFollower ? Theme.of(context).primaryColor : ColorManager.blue,
+          border: Border.all(
+              color: Theme.of(context).bottomAppBarColor,
+              width: isThatFollower ? 1.0 : 0),
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+                fontSize: 17.0,
+                color: isThatFollower
+                    ? Theme.of(context).focusColor
+                    : ColorManager.white,
+                fontWeight: FontWeight.w500),
+          ),
         ),
       ),
     );
