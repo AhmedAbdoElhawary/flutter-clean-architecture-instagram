ab95bbccd8f28d1606b569f2f99c5a0683af6620
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-07-12 04:00:20 +0200

    restructure GetsPostInfoAndDisplay

--- lib/presentation/widgets/global/custom_widgets/get_post_info.dart
+++ lib/presentation/widgets/global/custom_widgets/get_post_info.dart
@@ -1,37 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:instagram/presentation/cubit/postInfoCubit/post_cubit.dart';
+import 'package:instagram/presentation/widgets/belong_to/comments_w/build_comments.dart';
+import 'package:instagram/presentation/widgets/global/custom_widgets/custom_app_bar.dart';
 import 'package:instagram/presentation/widgets/global/custom_widgets/custom_posts_display.dart';
 
 class GetsPostInfoAndDisplay extends StatelessWidget {
   final String postId;
-  const GetsPostInfoAndDisplay({Key? key,required this.postId}) : super(key: key);
+  final String appBarText;
+
+  const GetsPostInfoAndDisplay(
+      {Key? key, required this.postId, required this.appBarText})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<PostCubit,PostState>(bloc:PostCubit.get(context)..getPostsInfo(
-        postsIds: [postId],
-        isThatMyPosts: false),
-      buildWhen:  (previous, current) {
-        if (previous != current && current is CubitPostFailed) {
-          return true;
-        }
-        return previous != current &&
-            current is CubitPostsInfoLoaded ;
-      },builder: (context, state) {
-        if (state is CubitPostsInfoLoaded) {
-          return CustomPostsDisplay(
-            postsInfo: state.postsInfo,
-            isThatProfile: false,
-            showCatchUp: false,
-          );
-        } else {
-          return CircularProgressIndicator(
-            strokeWidth: 1.5,
-            color: Theme.of(context).iconTheme.color,
-            backgroundColor: Theme.of(context).dividerColor,
-          );
-        }
-    },);
+    return Scaffold(
+      appBar: CustomAppBar.oneTitleAppBar(context, appBarText,
+          logoOfInstagram: true),
+      body: BlocBuilder<PostCubit, PostState>(
+        bloc: PostCubit.get(context)
+          ..getPostsInfo(postsIds: [postId], isThatMyPosts: false),
+        buildWhen: (previous, current) {
+          if (previous != current && current is CubitPostFailed) {
+            return true;
+          }
+          return previous != current && current is CubitPostsInfoLoaded;
+        },
+        builder: (context, state) {
+          if (state is CubitPostsInfoLoaded) {
+            if (state.postsInfo[0].comments.length < 10) {
+              return BuildComments(
+                postInfo: state.postsInfo[0],
+                showImage: true,
+              );
+            } else {
+              return CustomPostsDisplay(
+                postsInfo: state.postsInfo,
+                showCatchUp: false,
+              );
+            }
+          } else {
+            return Center(
+              child: CircularProgressIndicator(
+                strokeWidth: 1.5,
+                color: Theme.of(context).iconTheme.color,
+                backgroundColor: Theme.of(context).dividerColor,
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
