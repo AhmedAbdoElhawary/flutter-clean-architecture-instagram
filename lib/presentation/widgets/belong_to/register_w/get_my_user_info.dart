28f840dc7434ccc87dc42d035d22888317566069
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-07-13 22:20:43 +0200

    reformat the code

--- lib/presentation/widgets/belong_to/register_w/get_my_user_info.dart
+++ lib/presentation/widgets/belong_to/register_w/get_my_user_info.dart
@@ -1,23 +1,27 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:instagram/core/app_prefs.dart';
 import 'package:instagram/core/utility/constant.dart';
 import 'package:instagram/core/utility/injector.dart';
 import 'package:instagram/presentation/cubit/firestoreUserInfoCubit/user_info_cubit.dart';
-import 'package:instagram/presentation/screens/main_screen.dart';
+import 'package:instagram/presentation/screens/mobile_screen_layout.dart';
 import 'package:instagram/core/functions/toast_show.dart';
+import 'package:instagram/presentation/screens/responsive_layout.dart';
+import 'package:instagram/presentation/screens/web_screen_layout.dart';
 
-class GetMyPersonalId extends StatefulWidget {
+class GetMyPersonalInfo extends StatefulWidget {
   final String myPersonalId;
-  const GetMyPersonalId({Key? key, required this.myPersonalId})
+  const GetMyPersonalInfo({Key? key, required this.myPersonalId})
       : super(key: key);
 
   @override
-  State<GetMyPersonalId> createState() => _GetMyPersonalIdState();
+  State<GetMyPersonalInfo> createState() => _GetMyPersonalInfoState();
 }
 
-class _GetMyPersonalIdState extends State<GetMyPersonalId> {
+class _GetMyPersonalInfoState extends State<GetMyPersonalInfo> {
   bool isHeMovedToHome = false;
   final AppPreferences _appPreferences = injector<AppPreferences>();
 
@@ -29,28 +33,30 @@ class _GetMyPersonalIdState extends State<GetMyPersonalId> {
 
   @override
   Widget build(BuildContext context) {
-    return Builder(builder: (context) {
-      return BlocBuilder<FirestoreUserInfoCubit, FirestoreUserInfoState>(
-        bloc: FirestoreUserInfoCubit.get(context)
-          ..getUserInfo(widget.myPersonalId),
-        builder: (context, userState) {
-          WidgetsBinding.instance.addPostFrameCallback((_) async {
-            if (userState is CubitMyPersonalInfoLoaded) {
-              if (!isHeMovedToHome) {
-                myPersonalId = widget.myPersonalId;
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => MainScreen(myPersonalId)));
-              }
-              isHeMovedToHome = true;
-            } else if (userState is CubitGetUserInfoFailed) {
-              ToastShow.toastStateError(userState);
+    return BlocBuilder<FirestoreUserInfoCubit, FirestoreUserInfoState>(
+      bloc: FirestoreUserInfoCubit.get(context)
+        ..getUserInfo(widget.myPersonalId),
+      builder: (context, userState) {
+        WidgetsBinding.instance.addPostFrameCallback((_) async {
+          if (userState is CubitMyPersonalInfoLoaded) {
+            if (!isHeMovedToHome) {
+              myPersonalId = widget.myPersonalId;
+              Get.offAll(
+                ResponsiveLayout(
+                  mobileScreenLayout: MobileScreenLayout(myPersonalId),
+                  webScreenLayout: const WebScreenLayout(),
+                ),
+              );
             }
-          });
-          return Container(
-            color: Theme.of(context).primaryColor,
-          );
-        },
-      );
-    });
+            isHeMovedToHome = true;
+          } else if (userState is CubitGetUserInfoFailed) {
+            ToastShow.toastStateError(userState);
+          }
+        });
+        return Container(
+          color: Theme.of(context).primaryColor,
+        );
+      },
+    );
   }
 }
