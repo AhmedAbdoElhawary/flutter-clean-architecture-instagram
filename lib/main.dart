87ee1f35d0218acc71384a7f05b1ce430bf6ae53
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-07-13 22:14:08 +0200

    initialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:instagram/core/private_keys.dart';
 import 'package:instagram/core/resources/langauge_manager.dart';
 import 'package:instagram/material_app.dart';
-import 'package:photo_manager/photo_manager.dart';
+// import 'package:photo_manager/photo_manager.dart' if (dart.library.html) '';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'core/utility/injector.dart';
 
@@ -25,14 +28,33 @@ EasyLocalization easyLocalization(Widget myApp) {
 
 Future<SharedPreferences> init() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options:  FirebaseOptions(
+        // I deleted private_keys in github project,
+        // so create your own firebase project and add your web private keys here and in web/index.html.
+        apiKey: apiKey,
+        appId: appId,
+        messagingSenderId: messagingSenderId,
+        projectId: projectId,
+        storageBucket: storageBucket,
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   await EasyLocalization.ensureInitialized();
-  await PhotoManager.requestPermissionExtend();
-  await Firebase.initializeApp();
+  // if (!kIsWeb) {
+  //   await PhotoManager.requestPermissionExtend();
+  // }
   await initializeDependencies();
   await GetStorage.init();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitDown,
-    DeviceOrientation.portraitUp,
-  ]);
+  if (!kIsWeb) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   return await SharedPreferences.getInstance();
 }
