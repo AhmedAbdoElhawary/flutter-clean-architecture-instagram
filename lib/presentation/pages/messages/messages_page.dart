99f4064b80eaacd3eecd0135961c52759b627c68
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-07-17 23:10:52 +0200

    move the common function in ListOfMessages

--- lib/presentation/pages/messages/messages_page.dart
+++ lib/presentation/pages/messages/messages_page.dart
@@ -1,108 +1,15 @@
-import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:instagram/core/functions/date_of_now.dart';
-import 'package:instagram/core/resources/color_manager.dart';
-import 'package:instagram/core/resources/strings_manager.dart';
-import 'package:instagram/core/resources/styles_manager.dart';
 import 'package:instagram/core/utility/constant.dart';
-import 'package:instagram/core/utility/injector.dart';
-import 'package:instagram/data/models/message.dart';
-import 'package:instagram/data/models/sender_info.dart';
-import 'package:instagram/presentation/cubit/firestoreUserInfoCubit/message/bloc/message_bloc.dart';
-import 'package:instagram/presentation/cubit/firestoreUserInfoCubit/users_info_cubit.dart';
-import 'package:instagram/presentation/pages/messages/chatting_page.dart';
-import 'package:instagram/presentation/widgets/global/circle_avatar_image/circle_avatar_of_profile_image.dart';
-import 'package:instagram/presentation/widgets/global/custom_widgets/custom_circular_progress.dart';
-import 'package:instagram/core/functions/toast_show.dart';
+import 'package:instagram/presentation/widgets/belong_to/messages_w/list_of_messages.dart';
 
 class MessagesPage extends StatelessWidget {
   const MessagesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-    final bodyHeight = mediaQuery.size.height -
-        AppBar().preferredSize.height -
-        mediaQuery.padding.top;
     return Scaffold(
-      appBar:isThatMobile? buildAppBar(context):null,
-      body: BlocBuilder<UsersInfoCubit, UsersInfoState>(
-        bloc: UsersInfoCubit.get(context)
-          ..getChatUsersInfo(userId: myPersonalId),
-        builder: (context, state) {
-          if (state is CubitGettingChatUsersInfoLoaded) {
-            List<SenderInfo> usersInfo = state.usersInfo;
-            return ListView.separated(
-                itemBuilder: (context, index) {
-                  String hash = "${usersInfo[index].userInfo!.userId.hashCode}";
-                  Message theLastMessage = usersInfo[index].lastMessage;
-
-                  return ListTile(
-                    title: Text(
-                      usersInfo[index].userInfo!.name,
-                      style:
-                          getNormalStyle(color: Theme.of(context).focusColor),
-                    ),
-                    subtitle: Row(
-                      children: [
-                        Expanded(
-                          child: Text(
-                              theLastMessage.message.isEmpty
-                                  ? (theLastMessage.imageUrl.isEmpty
-                                      ? StringsManager.recordedSent.tr()
-                                      : StringsManager.photoSent.tr())
-                                  : theLastMessage.message,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 1,
-                              style: getNormalStyle(color: ColorManager.grey)),
-                        ),
-                        const SizedBox(width: 5),
-                        Text(
-                            DateOfNow.commentsDateOfNow(
-                                theLastMessage.datePublished),
-                            style: getNormalStyle(color: ColorManager.grey)),
-                      ],
-                    ),
-                    leading: Hero(
-                      tag: hash,
-                      child: CircleAvatarOfProfileImage(
-                        bodyHeight: bodyHeight * 0.85,
-                        hashTag: hash,
-                        userInfo: usersInfo[index].userInfo!,
-                      ),
-                    ),
-                    onTap: () {
-                      Navigator.of(
-                        context,
-                        rootNavigator: true,
-                      ).push(CupertinoPageRoute(
-                        builder: (context) => BlocProvider<MessageBloc>(
-                          create: (context) => injector<MessageBloc>(),
-                          child: ChattingPage(
-                            userInfo: usersInfo[index].userInfo!,
-                          ),
-                        ),
-                        maintainState: false,
-                      ));
-                    },
-                  );
-                },
-                itemCount: usersInfo.length,
-                separatorBuilder: (BuildContext context, int index) =>
-                    const Divider());
-          } else if (state is CubitGettingSpecificUsersFailed) {
-            ToastShow.toastStateError(state);
-            return Text(
-              StringsManager.somethingWrong,
-              style: getNormalStyle(color: Theme.of(context).focusColor),
-            );
-          } else {
-            return const ThineCircularProgress();
-          }
-        },
-      ),
+      appBar: isThatMobile ? buildAppBar(context) : null,
+      body: const SingleChildScrollView(child: ListOfMessages()),
     );
   }
 
