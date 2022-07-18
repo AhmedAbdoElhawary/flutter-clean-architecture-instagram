464c20c529bbd449149fac9db3427a62b6af9ca7
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-07-18 22:50:37 +0200

    replace File to Uint8List

--- lib/presentation/customPackages/audio_recorder/sound_recorder_when_locked_design.dart
+++ lib/presentation/customPackages/audio_recorder/sound_recorder_when_locked_design.dart
@@ -1,4 +1,4 @@
-import 'package:universal_io/io.dart';
+import 'dart:io';
 import 'dart:async';
 import 'package:instagram/core/resources/color_manager.dart';
 import 'package:flutter/material.dart';
