18e223ea65acfdd019ed2e84442d435e206c5853
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-07-18 22:48:23 +0200

    replace File to Uint8List

--- lib/core/functions/compress_image.dart
+++ lib/core/functions/compress_image.dart
@@ -1,33 +1,13 @@
-import 'package:universal_io/io.dart';
+import 'dart:typed_data';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
-import 'package:video_compress/video_compress.dart';
 
-Future<File?> compressImage(File file) async {
-  final filePath = file.absolute.path;
-
-  final lastIndex = filePath.lastIndexOf(RegExp(r'.jp'));
-  final split = filePath.substring(0, (lastIndex));
-  final outPath = "${split}_out${filePath.substring(lastIndex)}";
-  if (file.lengthSync() > 200000) {
-    File? result = await FlutterImageCompress.compressAndGetFile(
-      file.absolute.path,
-      outPath,
-      quality: file.lengthSync() > 5000000 ? 90 : 72,
+Future<Uint8List?> compressImage(Uint8List file) async {
+  if (file.lengthInBytes > 200000) {
+    Uint8List? result = await FlutterImageCompress.compressWithList(
+      file,
+      quality: file.lengthInBytes > 5000000 ? 90 : 72,
     );
     return result;
-  } else {
-    return file;
-  }
-}
-
-Future<File?> compressVideo(File file) async {
-  if (file.lengthSync() > 500000) {
-    MediaInfo? result = await VideoCompress.compressVideo(
-      file.path,
-      quality: VideoQuality.MediumQuality,
-      includeAudio: true,
-    );
-    return result!.file;
   } else {
     return file;
   }
