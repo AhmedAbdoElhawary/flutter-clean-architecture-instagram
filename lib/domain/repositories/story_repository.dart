798de3a1c3d33b22b20ac29d1d2847aeb983eea9
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-07-18 22:50:42 +0200

    replace File to Uint8List

--- lib/domain/repositories/story_repository.dart
+++ lib/domain/repositories/story_repository.dart
@@ -1,9 +1,9 @@
-import 'package:universal_io/io.dart';
+import 'dart:typed_data';
 import 'package:instagram/data/models/story.dart';
 import 'package:instagram/data/models/user_personal_info.dart';
 
 abstract class FirestoreStoryRepository {
-  Future<String> createStory({required Story storyInfo, required File file});
+  Future<String> createStory({required Story storyInfo, required Uint8List file});
   Future<List<UserPersonalInfo>> getStoriesInfo(
       {required List<dynamic> usersIds});
   Future<UserPersonalInfo> getSpecificStoriesInfo(
