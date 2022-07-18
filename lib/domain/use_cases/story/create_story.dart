f139788e140c337c91319c70bbfb074ac47fae4d
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-07-18 22:49:07 +0200

    replace File to Uint8List

--- lib/domain/use_cases/story/create_story.dart
+++ lib/domain/use_cases/story/create_story.dart
@@ -1,15 +1,15 @@
-import 'package:universal_io/io.dart';
+import 'dart:typed_data';
 import 'package:instagram/core/use_case/use_case.dart';
 import 'package:instagram/data/models/story.dart';
 import 'package:instagram/domain/repositories/story_repository.dart';
 
-class CreateStoryUseCase implements UseCaseTwoParams<String, Story, File> {
+class CreateStoryUseCase implements UseCaseTwoParams<String, Story, Uint8List> {
   final FirestoreStoryRepository _createStoryRepository;
 
   CreateStoryUseCase(this._createStoryRepository);
 
   @override
-  Future<String> call({required Story paramsOne, required File paramsTwo}) {
+  Future<String> call({required Story paramsOne, required Uint8List paramsTwo}) {
     return _createStoryRepository.createStory(
         storyInfo: paramsOne, file: paramsTwo);
   }
