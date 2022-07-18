7fb2da8ebd46e6caf2e16ab6a46bfc51735af1a7
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-07-18 20:52:33 +0200

    change io to universal_io/io

--- lib/presentation/pages/profile/personal_profile_page.dart
+++ lib/presentation/pages/profile/personal_profile_page.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'dart:io';
+import 'package:universal_io/io.dart';
 
 import 'package:custom_gallery_display/custom_gallery_display.dart';
 import 'package:easy_localization/easy_localization.dart';
