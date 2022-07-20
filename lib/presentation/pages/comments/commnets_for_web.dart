15675db5a261bdbd548b1ee5d4588560310071d4
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-07-20 02:28:35 +0200

    create CommentsForWeb

---
+++ lib/presentation/pages/comments/commnets_for_web.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommentsForWeb extends StatelessWidget {
+  const CommentsForWeb({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
