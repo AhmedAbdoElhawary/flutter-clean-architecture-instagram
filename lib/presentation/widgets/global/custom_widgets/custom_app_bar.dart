e64730f291d64f4e01adc96ed9ce37fbef63b4b7
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-07-31 14:54:42 +0200

    update the app bar

--- lib/presentation/widgets/global/custom_widgets/custom_app_bar.dart
+++ lib/presentation/widgets/global/custom_widgets/custom_app_bar.dart
@@ -1,18 +1,12 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram/config/routes/app_routes.dart';
 import 'package:instagram/core/widgets/svg_pictures.dart';
 import 'package:instagram/core/resources/assets_manager.dart';
-import 'package:instagram/core/resources/strings_manager.dart';
 import 'package:instagram/core/resources/styles_manager.dart';
-import 'package:instagram/core/utility/injector.dart';
 import 'package:instagram/data/models/user_personal_info.dart';
-import 'package:instagram/presentation/cubit/firestoreUserInfoCubit/users_info_cubit.dart';
 import 'package:instagram/presentation/pages/activity/activity_for_mobile.dart';
-import 'package:instagram/presentation/pages/messages/messages_page.dart';
 import 'package:instagram/presentation/widgets/belong_to/profile_w/custom_gallery/create_new_story.dart';
 import 'package:instagram/presentation/widgets/global/custom_widgets/custom_gallery_display.dart';
 import 'package:instagram/presentation/widgets/global/custom_widgets/custom_network_image_display.dart';
@@ -25,98 +19,6 @@ class CustomAppBar {
       iconTheme: IconThemeData(color: Theme.of(context).focusColor),
       title: const InstagramLogo(),
       actions: [
-        Padding(
-          padding: const EdgeInsetsDirectional.only(end: 10.0),
-          child: PopupMenuButton<int>(
-            position: PopupMenuPosition.under,
-            elevation: 20,
-            color: Theme.of(context).splashColor,
-            offset: const Offset(90, 8),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-            child: SvgPicture.asset(
-              IconsAssets.add2Icon,
-              color: Theme.of(context).focusColor,
-              height: 22.5,
-            ),
-            onSelected: (item) => onSelected(context, item),
-            itemBuilder: (context) => [
-              PopupMenuItem<int>(
-                value: 0,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        StringsManager.post.tr(),
-                        style:
-                            getMediumStyle(color: Theme.of(context).focusColor),
-                      ),
-                    ),
-                    const SizedBox(width: 25),
-                    const Icon(Icons.grid_on_sharp),
-                  ],
-                ),
-              ),
-              PopupMenuItem<int>(
-                value: 1,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        StringsManager.story.tr(),
-                        style:
-                            getMediumStyle(color: Theme.of(context).focusColor),
-                      ),
-                    ),
-                    SvgPicture.asset(
-                      IconsAssets.addInstagramStoryIcon,
-                      color: Theme.of(context).focusColor,
-                      height: 25,
-                    ),
-                  ],
-                ),
-              ),
-              PopupMenuItem<int>(
-                value: 2,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        StringsManager.reel.tr(),
-                        style:
-                            getMediumStyle(color: Theme.of(context).focusColor),
-                      ),
-                    ),
-                    SvgPicture.asset(
-                      IconsAssets.videoIcon,
-                      color: Theme.of(context).focusColor,
-                      height: 25,
-                    ),
-                  ],
-                ),
-              ),
-              PopupMenuItem<int>(
-                value: 3,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        StringsManager.live.tr(),
-                        style:
-                            getMediumStyle(color: Theme.of(context).focusColor),
-                      ),
-                    ),
-                    SvgPicture.asset(
-                      IconsAssets.instagramHighlightStoryIcon,
-                      color: Theme.of(context).focusColor,
-                      height: 25,
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
         IconButton(
           icon: SvgPicture.asset(
             IconsAssets.favorite,
@@ -125,20 +27,6 @@ class CustomAppBar {
           ),
           onPressed: () {
             pushToPage(context, page: const ActivityPage(), withoutRoot: false);
-          },
-        ),
-        IconButton(
-          icon: SvgPicture.asset(
-            IconsAssets.messengerIcon,
-            color: Theme.of(context).focusColor,
-            height: 22.5,
-          ),
-          onPressed: () {
-            pushToPage(context,
-                page: BlocProvider<UsersInfoCubit>(
-                  create: (context) => injector<UsersInfoCubit>(),
-                  child: const MessagesPageForMobile(),
-                ));
           },
         ),
         const SizedBox(width: 5),
