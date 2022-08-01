21fc88ecc8e3e24868849ef70605a7d5e9578e57
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-01 15:52:57 +0200

    delete unuseful import

--- lib/presentation/pages/video/videos_page.dart
+++ lib/presentation/pages/video/videos_page.dart
@@ -1,6 +1,5 @@
 import 'dart:typed_data';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
