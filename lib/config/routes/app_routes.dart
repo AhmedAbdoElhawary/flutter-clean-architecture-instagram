aeba49aec455704214067a3bff7da6872e2cb52f
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-01 10:13:34 +0200

    add withoutPageTransition as a parameter in pushToPage

--- lib/config/routes/app_routes.dart
+++ lib/config/routes/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/get_navigation/src/routes/transitions_type.dart';
@@ -7,15 +8,19 @@ import 'package:instagram/presentation/screens/web_screen_layout.dart';
 
 /// I don't use routes methods because there is a lot of run time error.
 /// I use normal Navigator because i don't know how to make Get.to without root.
-Future pushToPage(BuildContext context,
-    {required Widget page, bool withoutRoot = true}) async {
+Future pushToPage(
+  BuildContext context, {
+  required Widget page,
+  bool withoutRoot = true,
+  bool withoutPageTransition = false,
+}) async {
   if (isThatMobile) {
-    return Navigator.of(context, rootNavigator: withoutRoot)
-        .push(CupertinoPageRoute(
-            builder: (context) {
-              return page;
-            },
-            maintainState: !withoutRoot));
+    PageRoute route = withoutPageTransition
+        ? MaterialPageRoute(
+            builder: (context) => page, maintainState: !withoutRoot)
+        : CupertinoPageRoute(
+            builder: (context) => page, maintainState: !withoutRoot);
+    return Navigator.of(context, rootNavigator: withoutRoot).push(route);
   } else {
     return Get.to(WebScreenLayout(body: page),
         transition: Transition.noTransition);
