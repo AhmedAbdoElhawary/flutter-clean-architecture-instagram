0690bf5883ae4311dd29559cc4ffcd6e8186cfb2
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-01 15:51:45 +0200

    delete unuseful import

--- lib/presentation/pages/time_line/my_own_time_line/home_page.dart
+++ lib/presentation/pages/time_line/my_own_time_line/home_page.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:instagram/config/routes/app_routes.dart';
