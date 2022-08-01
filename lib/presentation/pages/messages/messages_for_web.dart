c0a8078a4f4677965397857b3eea1215cc1cf1d5
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-01 15:52:03 +0200

    delete unuseful import

--- lib/presentation/pages/messages/messages_for_web.dart
+++ lib/presentation/pages/messages/messages_for_web.dart
@@ -12,6 +12,7 @@ import 'package:instagram/presentation/widgets/belong_to/messages_w/chat_message
 import 'package:instagram/presentation/widgets/belong_to/messages_w/list_of_messages.dart';
 import 'package:instagram/presentation/widgets/global/circle_avatar_image/circle_avatar_of_profile_image.dart';
 
+// ignore: must_be_immutable
 class MessagesForWeb extends StatefulWidget {
   UserPersonalInfo? selectedTextingUser;
 
