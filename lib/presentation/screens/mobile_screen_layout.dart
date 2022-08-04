47cb0c9970ce431e798f6daff3f4e4fbb54a0abe
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-04 20:07:04 +0200

    return the old style of instagram

--- lib/presentation/screens/mobile_screen_layout.dart
+++ lib/presentation/screens/mobile_screen_layout.dart
@@ -2,18 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:instagram/config/routes/app_routes.dart';
 import 'package:instagram/core/resources/assets_manager.dart';
+import 'package:instagram/core/resources/color_manager.dart';
 import 'package:instagram/core/utility/injector.dart';
-import 'package:instagram/presentation/cubit/firestoreUserInfoCubit/users_info_cubit.dart';
 import 'package:instagram/presentation/cubit/postInfoCubit/post_cubit.dart';
-import 'package:instagram/presentation/pages/messages/messages_page_for_mobile.dart';
 import 'package:instagram/presentation/pages/profile/personal_profile_page.dart';
 import 'package:instagram/presentation/pages/shop/shop_page.dart';
 import 'package:instagram/presentation/pages/time_line/all_user_time_line/all_users_time_line.dart';
 import 'package:instagram/presentation/pages/time_line/my_own_time_line/home_page.dart';
+import 'package:instagram/presentation/pages/video/videos_page.dart';
 import 'package:instagram/presentation/widgets/belong_to/screens_w.dart';
-import 'package:instagram/presentation/widgets/global/custom_widgets/custom_gallery_display.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   final String userId;
@@ -24,10 +22,9 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  ValueNotifier<bool> playReelVideo = ValueNotifier(false);
   ValueNotifier<bool> playHomeVideo = ValueNotifier(false);
   CupertinoTabController controller = CupertinoTabController();
-  ValueNotifier<bool> stopReelVideo = ValueNotifier(false);
-
   @override
   void initState() {
     super.initState();
@@ -35,37 +32,43 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(
-            backgroundColor: Theme.of(context).primaryColor,
-            height: 40,
-            items: [
-              navigationBarItem(IconsAssets.home),
-              navigationBarItem(IconsAssets.messengerIcon),
-              navigationBarItem(IconsAssets.addIcon, littleSmall: true),
-              navigationBarItem(IconsAssets.search),
-              personalImageItem(),
-            ]),
-        controller: controller,
-        tabBuilder: (context, index) {
-          WidgetsBinding.instance.addPostFrameCallback((_) async {
-            stopReelVideo.value = controller.index != 0 ? true : false;
-            playHomeVideo.value = controller.index == 0 ? true : false;
-          });
+    return ValueListenableBuilder(
+      valueListenable: playReelVideo,
+      builder: (BuildContext context, bool value, __) {
+        return CupertinoTabScaffold(
+            tabBar: CupertinoTabBar(
+                backgroundColor:
+                    value ? ColorManager.black : Theme.of(context).primaryColor,
+                height: 40,
+                items: [
+                  navigationBarItem(IconsAssets.home, value),
+                  navigationBarItem(IconsAssets.search, value),
+                  navigationBarItem(IconsAssets.video, value, smallIcon: true),
+                  navigationBarItem(IconsAssets.shop, value),
+                  personalImageItem(),
+                ]),
+            controller: controller,
+            tabBuilder: (context, index) {
+              WidgetsBinding.instance.addPostFrameCallback((_) async {
+                playReelVideo.value = controller.index == 2 ? true : false;
+                playHomeVideo.value = controller.index == 0 ? true : false;
+              });
 
-          switch (index) {
-            case 0:
-              return homePage();
-            case 1:
-              return messagesPage();
-            case 2:
-              return galleryPage();
-            case 3:
-              return allUsersTimLinePage();
-            default:
-              return personalProfilePage();
-          }
-        });
+              switch (index) {
+                case 0:
+                  return homePage();
+                case 1:
+                  return allUsersTimLinePage();
+                case 2:
+                  return videoPage(value);
+                case 3:
+                  return shopPage();
+                default:
+                  return personalProfilePage();
+              }
+            });
+      },
+    );
   }
 
   CupertinoTabView allUsersTimLinePage() => CupertinoTabView(
@@ -88,34 +91,28 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
         ),
       );
 
-  Widget messagesPage() => CupertinoTabView(
-        builder: (context) => CupertinoPageScaffold(
-          child: BlocProvider<UsersInfoCubit>(
-            create: (context) => injector<UsersInfoCubit>(),
-            child: const MessagesPageForMobile(),
-          ),
-        ),
-      );
-  Widget galleryPage() => CupertinoTabView(
-    builder: (context) => const CupertinoPageScaffold(
-      child:CustomGalleryDisplay(),
-    ),
-  );
+  Widget videoPage(bool value) => CupertinoTabView(
+      builder: (context) => CupertinoPageScaffold(
+            child: BlocProvider<PostCubit>(
+              create: (context) => injector<PostCubit>(),
+              child: VideosPage(
+                stopVideo: ValueNotifier(value),
+              ),
+            ),
+          ));
 
   Widget homePage() => CupertinoTabView(
-        builder: (context) => CupertinoPageScaffold(child: home()),
-      );
-
-  BlocProvider<PostCubit> home() => BlocProvider<PostCubit>(
-        create: (context) => injector<PostCubit>(),
-        child: ValueListenableBuilder(
-          valueListenable: playHomeVideo,
-          builder: (context, bool playVideoValue, child) => HomePage(
-            userId: widget.userId,
-            playVideo: playVideoValue,
-            stopReelVideoValue: stopReelVideo,
+        builder: (context) => CupertinoPageScaffold(
+            child: BlocProvider<PostCubit>(
+          create: (context) => injector<PostCubit>(),
+          child: ValueListenableBuilder(
+            valueListenable: playHomeVideo,
+            builder: (context, bool playVideoValue, child) => HomePage(
+              userId: widget.userId,
+              playVideo: playVideoValue,
+            ),
           ),
-        ),
+        )),
       );
 
   BottomNavigationBarItem personalImageItem() {
@@ -124,21 +121,13 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
     );
   }
 
-  BottomNavigationBarItem navigationBarItem(String icon,
-      {bool littleSmall = false}) {
+  BottomNavigationBarItem navigationBarItem(String icon, bool value,
+      {bool smallIcon = false}) {
     return BottomNavigationBarItem(
-      icon: GestureDetector(
-        onTap: littleSmall
-            ? () {
-                pushToPage(context,
-                    page: const CustomGalleryDisplay(), withoutRoot: false);
-              }
-            : null,
-        child: SvgPicture.asset(
-          icon,
-          height: littleSmall ? 23 : 25,
-          color: Theme.of(context).focusColor,
-        ),
+      icon: SvgPicture.asset(
+        icon,
+        height: smallIcon ? 23 : 25,
+        color: value ? ColorManager.white : Theme.of(context).focusColor,
       ),
     );
   }
