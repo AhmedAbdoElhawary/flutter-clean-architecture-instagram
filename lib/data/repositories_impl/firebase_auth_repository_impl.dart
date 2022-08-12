4278a1df7eb6e8d87381630dc838799bbf8a8133
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-12 23:17:03 +0200

    add some parameters to signOut

--- lib/data/repositories_impl/firebase_auth_repository_impl.dart
+++ lib/data/repositories_impl/firebase_auth_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:instagram/core/resources/strings_manager.dart';
+import 'package:instagram/data/datasourses/remote/notification/firebase_notification.dart';
 import 'package:instagram/domain/entities/registered_user.dart';
 import 'package:instagram/domain/entities/unregistered_user.dart';
 import 'package:instagram/domain/repositories/auth_repository.dart';
@@ -18,22 +19,25 @@ class FirebaseAuthRepositoryImpl implements FirebaseAuthRepository {
   }
 
   @override
-  Future<void> signOut() {
+  Future<void> signOut({required String userId, required String? deviceToken}) async {
     try {
-      return FirebaseAuthentication.signOut();
+      await FirebaseAuthentication.signOut();
+      await FirestoreNotification.deleteDeviceToken(userId: userId, deviceToken: deviceToken);
+      return ;
     } catch (e) {
       return Future.error(e.toString());
     }
   }
 
   @override
-  Future<User> signUp(UnRegisteredUser newUserInfo) {
+  Future<User> signUp(UnRegisteredUser newUserInfo) async {
     try {
       if (newUserInfo.password != newUserInfo.confirmPassword) {
         throw StringsManager.passwordWrong.tr();
       }
-      return FirebaseAuthentication.signUp(
+      User userId = await FirebaseAuthentication.signUp(
           email: newUserInfo.email, password: newUserInfo.password);
+      return userId;
     } catch (e) {
       return Future.error(e.toString());
     }
