17b3d1bcb54c3c869ab186edc547ea824a518980
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-14 23:42:05 +0200

    create CallingRoomsRepository

---
+++ lib/domain/repositories/calling_rooms_repository.dart
@@ -0,0 +1,20 @@
+import 'package:instagram/data/models/user_personal_info.dart';
+import 'package:instagram/domain/entities/calling_status.dart';
+
+abstract class CallingRoomsRepository {
+  Future<String> createCallingRoom(
+      {required UserPersonalInfo myPersonalInfo,
+      required String callToThisUserId});
+
+   Stream<bool> getCallingStatus({required String userId});
+
+  Future<String> joinToRoom(
+      {required String channelId, required UserPersonalInfo userInfo});
+
+  Future<void> cancelJoiningToRoom(String userId);
+
+  Future<List<UserInfoInCallingRoom>> getUsersInfoInThisRoom(
+      {required String channelId});
+  Future<void> deleteTheRoom({required String channelId,required String userId});
+
+  }
