9bed7fc36ecd0d57933b7e89ff21f6fdf3a88a22
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-15 15:33:27 +0200

    create GetMyInfoUseCase

---
+++ lib/domain/use_cases/user/my_personal_info.dart
@@ -0,0 +1,14 @@
+import 'package:instagram/core/use_case/use_case.dart';
+import 'package:instagram/data/models/user_personal_info.dart';
+import 'package:instagram/domain/repositories/user_repository.dart';
+
+class GetMyInfoUseCase implements StreamUseCase<UserPersonalInfo, void> {
+  final FirestoreUserRepository _getMyInfoRepository;
+
+  GetMyInfoUseCase(this._getMyInfoRepository);
+
+  @override
+  Stream<UserPersonalInfo> call({required void params}) {
+    return _getMyInfoRepository.getMyPersonalInfo();
+  }
+}
