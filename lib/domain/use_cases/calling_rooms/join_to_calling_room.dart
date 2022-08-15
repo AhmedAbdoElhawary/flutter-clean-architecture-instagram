35c78f453daceaecafcffc3db67018f3828bba66
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-15 15:34:15 +0200

    create JoinToCallingRoomUseCase

---
+++ lib/domain/use_cases/calling_rooms/join_to_calling_room.dart
@@ -0,0 +1,15 @@
+import 'package:instagram/data/models/user_personal_info.dart';
+import 'package:instagram/domain/repositories/calling_rooms_repository.dart';
+import 'package:instagram/core/use_case/use_case.dart';
+
+class JoinToCallingRoomUseCase
+    extends UseCaseTwoParams<String, String, UserPersonalInfo> {
+  final CallingRoomsRepository _callingRoomsRepo;
+  JoinToCallingRoomUseCase(this._callingRoomsRepo);
+  @override
+  Future<String> call(
+      {required String paramsOne, required UserPersonalInfo paramsTwo}) async {
+    return await _callingRoomsRepo.joinToRoom(
+        channelId: paramsOne, userInfo: paramsTwo);
+  }
+}
