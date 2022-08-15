0e46c14392ad8efdeb3459f234a09af8b49bedd6
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-15 15:33:05 +0200

    create GetCallingStatusUseCase

---
+++ lib/domain/use_cases/calling_rooms/get_calling_status.dart
@@ -0,0 +1,11 @@
+import 'package:instagram/domain/repositories/calling_rooms_repository.dart';
+import 'package:instagram/core/use_case/use_case.dart';
+
+class GetCallingStatusUseCase extends StreamUseCase<bool, String> {
+  final CallingRoomsRepository _callingRoomsRepo;
+  GetCallingStatusUseCase(this._callingRoomsRepo);
+  @override
+  Stream<bool> call({required String params}) {
+    return _callingRoomsRepo.getCallingStatus(userId: params);
+  }
+}
