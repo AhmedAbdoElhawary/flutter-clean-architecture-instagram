ec27dad9151bf2f5ab9b01ef8ab4d470fc352f86
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-17 16:28:22 +0200

    reformat the code

--- lib/domain/use_cases/user/getUserInfo/get_user_info_usecase.dart
+++ lib/domain/use_cases/user/getUserInfo/get_user_info_usecase.dart
@@ -2,13 +2,16 @@ import 'package:instagram/core/use_case/use_case.dart';
 import '../../../../data/models/user_personal_info.dart';
 import '../../../repositories/user_repository.dart';
 
-class GetUserInfoUseCase implements UseCaseTwoParams<UserPersonalInfo, String,bool> {
+class GetUserInfoUseCase
+    implements UseCaseTwoParams<UserPersonalInfo, String, bool> {
   final FirestoreUserRepository _addNewUserRepository;
 
   GetUserInfoUseCase(this._addNewUserRepository);
 
   @override
-  Future<UserPersonalInfo> call({required String paramsOne, required bool paramsTwo}) {
-    return _addNewUserRepository.getPersonalInfo(userId: paramsOne,getDeviceToken: paramsTwo);
+  Future<UserPersonalInfo> call(
+      {required String paramsOne, required bool paramsTwo}) {
+    return _addNewUserRepository.getPersonalInfo(
+        userId: paramsOne, getDeviceToken: paramsTwo);
   }
 }
