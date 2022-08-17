b77afffe77b173c663ac43b56d8b07005362291e
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-17 16:28:15 +0200

    reformat the code

--- lib/domain/use_cases/user/getUserInfo/get_all_users_info.dart
+++ lib/domain/use_cases/user/getUserInfo/get_all_users_info.dart
@@ -3,13 +3,14 @@ import 'package:instagram/core/use_case/use_case.dart';
 import '../../../../data/models/user_personal_info.dart';
 import '../../../repositories/user_repository.dart';
 
-class GetAllUsersUseCase implements UseCase<List<UserPersonalInfo>, UserPersonalInfo> {
+class GetAllUsersUseCase
+    implements UseCase<List<UserPersonalInfo>, UserPersonalInfo> {
   final FirestoreUserRepository _getAllUnFollowersUsersUseCase;
 
   GetAllUsersUseCase(this._getAllUnFollowersUsersUseCase);
 
   @override
   Future<List<UserPersonalInfo>> call({required UserPersonalInfo params}) {
-    return _getAllUnFollowersUsersUseCase.getAllUnFollowersUsers( params);
+    return _getAllUnFollowersUsersUseCase.getAllUnFollowersUsers(params);
   }
 }
