4a70b88de930196b143ead22835a6c45476e3694
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-17 16:51:04 +0200

    create CallingStatusBloc

---
+++ lib/presentation/cubit/callingRooms/bloc/calling_status_bloc.dart
@@ -0,0 +1,42 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instagram/domain/use_cases/calling_rooms/get_calling_status.dart';
+
+part 'calling_status_event.dart';
+part 'calling_status_state.dart';
+
+class CallingStatusBloc extends Bloc<CallingStatusEvent, CallingStatusState> {
+  final GetCallingStatusUseCase _getCallingStatusUseCase;
+
+  CallingStatusBloc(this._getCallingStatusUseCase)
+      : super(CallingStatusInitial());
+  static CallingStatusBloc get(BuildContext context) =>
+      BlocProvider.of(context);
+
+  @override
+  Stream<CallingStatusState> mapEventToState(
+    CallingStatusEvent event,
+  ) async* {
+    if (event is LoadCallingStatus) {
+      yield* _mapLoadInfoToState(event.userIAmCalling);
+    } else if (event is UpdateCallingStatus) {
+      yield* _mapUpdateInfoToState(event);
+    }
+  }
+
+  Stream<CallingStatusState> _mapLoadInfoToState(String userIAmCalling) async* {
+    _getCallingStatusUseCase.call(params: userIAmCalling).listen(
+      (myPersonalInfo) {
+        add(UpdateCallingStatus(myPersonalInfo));
+      },
+    ).onError((e) async* {
+      yield CallingStatusFailed(e.toString());
+    });
+  }
+
+  Stream<CallingStatusState> _mapUpdateInfoToState(
+      UpdateCallingStatus event) async* {
+    yield CallingStatusLoaded(callingStatus: event.callingStatus);
+  }
+}
