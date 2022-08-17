391e75d8c733e6564eca3f1b2327a668e58b9273
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-17 20:48:05 +0200

    solve video display bugs

--- lib/presentation/pages/video/play_this_video.dart
+++ lib/presentation/pages/video/play_this_video.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:instagram/core/resources/color_manager.dart';
+import 'package:instagram/data/models/post.dart';
+import 'package:instagram/presentation/widgets/global/custom_widgets/custom_network_image_display.dart';
 import 'package:video_player/video_player.dart';
 
 class PlayThisVideo extends StatefulWidget {
-  final String videoUrl;
+  final Post videoInfo;
   final bool play;
   final bool withoutSound;
-
   const PlayThisVideo({
     Key? key,
-    required this.videoUrl,
+    required this.videoInfo,
     this.withoutSound = false,
     required this.play,
   }) : super(key: key);
@@ -23,7 +24,7 @@ class PlayThisVideoState extends State<PlayThisVideo> {
 
   @override
   void initState() {
-    _controller = VideoPlayerController.network(widget.videoUrl);
+    _controller = VideoPlayerController.network(widget.videoInfo.postUrl);
     _initializeVideoPlayerFuture = _controller.initialize().then((_) {
       setState(() {});
     });
@@ -72,20 +73,30 @@ class PlayThisVideoState extends State<PlayThisVideo> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initializeVideoPlayerFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return AspectRatio(
-            aspectRatio: 0.65,
-            child: VideoPlayer(_controller),
-          );
-        } else {
-          return AspectRatio(
+    if (widget.videoInfo.coverOfVideoUrl.isNotEmpty && !widget.play) {
+      return AspectRatio(
+        aspectRatio: 0.65,
+        child: NetworkImageDisplay(
+          blurHash: widget.videoInfo.blurHash,
+          imageUrl: widget.videoInfo.coverOfVideoUrl,
+        ),
+      );
+    } else {
+      return FutureBuilder(
+        future: _initializeVideoPlayerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return AspectRatio(
               aspectRatio: 0.65,
-              child: Container(color: ColorManager.lowOpacityGrey));
-        }
-      },
-    );
+              child: VideoPlayer(_controller),
+            );
+          } else {
+            return AspectRatio(
+                aspectRatio: 0.65,
+                child: Container(color: ColorManager.lowOpacityGrey));
+          }
+        },
+      );
+    }
   }
 }
