b54fc53c5e0308e948ad90b5c730761aa479e91d
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-17 16:38:39 +0200

    reformat the code

--- lib/presentation/widgets/global/custom_widgets/custom_grid_view_display.dart
+++ lib/presentation/widgets/global/custom_widgets/custom_grid_view_display.dart
@@ -207,17 +207,18 @@ class _CustomGridViewDisplayState extends State<CustomGridViewDisplay> {
     customPostsInfo.removeWhere(
         (value) => value.postUid == widget.postClickedInfo.postUid);
     customPostsInfo.insert(0, widget.postClickedInfo);
-    Scaffold page = Scaffold(
+    Scaffold page= Scaffold(
       appBar: isThatMobile
           ? CustomAppBar.oneTitleAppBar(
-              context,
-              widget.isThatProfile
-                  ? StringsManager.posts.tr()
-                  : StringsManager.explore.tr())
+          context,
+          widget.isThatProfile
+              ? StringsManager.posts.tr()
+              : StringsManager.explore.tr())
           : null,
       body: CustomPostsDisplay(postsInfo: widget.postsInfo),
     );
-    pushToPage(context, page: page, withoutRoot: false);
+    pushToPage(context,
+        page: page,withoutRoot: false);
   }
 
   void onLongPressMoveUpdate(LongPressMoveUpdateDetails details) {
@@ -324,8 +325,6 @@ class _CustomGridViewDisplayState extends State<CustomGridViewDisplay> {
           userId: widget.postClickedInfo.publisherId,
         );
       }
-      if (!mounted) return;
-
       pushToPage(context, page: page, withoutRoot: false);
     }
     if (shareVisibility.value) draggableBottomSheet();
