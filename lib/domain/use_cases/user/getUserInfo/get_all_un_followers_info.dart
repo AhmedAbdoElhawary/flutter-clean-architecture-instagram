29270eda390ffb25a1391474608b886fc265d43b
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-19 19:44:02 +0200

    change the name of GetAllUsersUseCase to _WelcomeCards

--- lib/domain/use_cases/user/getUserInfo/get_all_un_followers_info.dart
+++ lib/domain/use_cases/user/getUserInfo/get_all_un_followers_info.dart
@@ -1,13 +1,12 @@
-// create interface from UseCase to get all users info
 import 'package:instagram/core/use_case/use_case.dart';
 import '../../../../data/models/user_personal_info.dart';
 import '../../../repositories/user_repository.dart';
 
-class GetAllUsersUseCase
+class GetAllUnFollowersUseCase
     implements UseCase<List<UserPersonalInfo>, UserPersonalInfo> {
   final FirestoreUserRepository _getAllUnFollowersUsersUseCase;
 
-  GetAllUsersUseCase(this._getAllUnFollowersUsersUseCase);
+  GetAllUnFollowersUseCase(this._getAllUnFollowersUsersUseCase);
 
   @override
   Future<List<UserPersonalInfo>> call({required UserPersonalInfo params}) {
