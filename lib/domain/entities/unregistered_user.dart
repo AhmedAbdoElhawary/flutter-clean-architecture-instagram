4c8f05518c6459eed8c5f40e47a051bc849e86d9
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-19 23:37:13 +0200

    delete UnRegisteredUser

--- lib/domain/entities/unregistered_user.dart
+++
@@ -1,8 +0,0 @@
-import 'package:instagram/domain/entities/registered_user.dart';
-
-class UnRegisteredUser extends RegisteredUser {
-  String confirmPassword;
-  UnRegisteredUser(
-      {String email = "", String password = "", this.confirmPassword = ""})
-      : super(email: email, password: password);
-}
--- lib/domain/use_cases/auth/sign_up_auth_usecase.dart
+++ lib/domain/use_cases/auth/sign_up_auth_usecase.dart
@@ -1,15 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:instagram/core/use_case/use_case.dart';
-import '../../entities/unregistered_user.dart';
+import 'package:instagram/domain/entities/registered_user.dart';
 import '../../repositories/auth_repository.dart';
 
-class SignUpAuthUseCase implements UseCase<User, UnRegisteredUser> {
+class SignUpAuthUseCase implements UseCase<User, RegisteredUser> {
   final FirebaseAuthRepository _firebaseAuthRepository;
 
   SignUpAuthUseCase(this._firebaseAuthRepository);
 
   @override
-  Future<User> call({required UnRegisteredUser params}) {
+  Future<User> call({required RegisteredUser params}) {
     return _firebaseAuthRepository.signUp(params);
   }
 }
