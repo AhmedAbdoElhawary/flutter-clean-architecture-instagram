373310c1f0abc33f08292aca17bd3ab76ddf8092
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-19 19:47:29 +0200

    create GetAllUsersUseCase

---
+++ lib/domain/use_cases/user/getUserInfo/get_all_users_info.dart
@@ -0,0 +1,15 @@
+import 'package:instagram/core/use_case/use_case.dart';
+import '../../../../data/models/user_personal_info.dart';
+import '../../../repositories/user_repository.dart';
+
+class GetAllUsersUseCase
+    implements StreamUseCase<List<UserPersonalInfo>, void> {
+  final FirestoreUserRepository _getAllUsersUseCase;
+
+  GetAllUsersUseCase(this._getAllUsersUseCase);
+
+  @override
+  Stream<List<UserPersonalInfo>> call({required void params}) {
+    return _getAllUsersUseCase.getAllUsers();
+  }
+}
