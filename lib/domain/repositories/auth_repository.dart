7bd20add17b849bf7d855f9c64af8e5f18c37812
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-19 23:35:20 +0200

    reforamt the code

--- lib/domain/repositories/auth_repository.dart
+++ lib/domain/repositories/auth_repository.dart
@@ -1,9 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:instagram/domain/entities/registered_user.dart';
-import '../entities/unregistered_user.dart';
 
 abstract class FirebaseAuthRepository {
-  Future<User> signUp(UnRegisteredUser newUserInfo);
+  Future<User> signUp(RegisteredUser newUserInfo);
   Future<User> logIn(RegisteredUser userInfo);
   Future<void> signOut({required String userId, required String? deviceToken});
 }
