ef42b06fa44847685e33889c3f20fc5ae694be34
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-21 19:41:23 +0200

    change the package of translation (tr() to tr)

--- lib/data/datasourses/remote/firebase_storage.dart
+++ lib/data/datasourses/remote/firebase_storage.dart
@@ -6,14 +6,25 @@ import 'package:path/path.dart';
 import 'dart:typed_data';
 
 class FirebaseStoragePost {
-  static Future<String> uploadFile(Uint8List postUint8List, String folderName,
-      {File? postFile}) async {
-    final fileName =
-        postFile != null ? basename(postFile.path) : DateTime.now().toString();
+  static Future<String> uploadFile({
+    required String folderName,
+    required File postFile,
+  }) async {
+    final fileName = basename(postFile.path);
     final destination = 'files/$folderName/$fileName';
     final ref = firebase_storage.FirebaseStorage.instance.ref(destination);
-    UploadTask uploadTask =
-        postFile != null ? ref.putFile(postFile) : ref.putData(postUint8List);
+    UploadTask uploadTask = ref.putFile(postFile);
+    String fileOfPostUrl =
+        await (await uploadTask.whenComplete(() {})).ref.getDownloadURL();
+    return fileOfPostUrl;
+  }
+
+  static Future<String> uploadData(
+      {required String folderName, required Uint8List data}) async {
+    final fileName = DateTime.now().toString();
+    final destination = 'data/$folderName/$fileName';
+    final ref = firebase_storage.FirebaseStorage.instance.ref(destination);
+    UploadTask uploadTask = ref.putData(data);
     String fileOfPostUrl =
         await (await uploadTask.whenComplete(() {})).ref.getDownloadURL();
     return fileOfPostUrl;
