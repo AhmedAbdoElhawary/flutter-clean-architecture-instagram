f24793e87395828b9ba891ecf595ce7a3998670d
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-23 00:56:14 +0200

    add pushNotification in DeviceNotification

--- lib/data/datasourses/remote/notification/device_notification.dart
+++ lib/data/datasourses/remote/notification/device_notification.dart
@@ -3,44 +3,52 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:instagram/core/private_keys.dart';
+import 'package:instagram/data/models/notification.dart';
+import 'package:instagram/data/models/push_notification.dart';
 
 class DeviceNotification {
-  static Future<void> sendPopupNotification({
-    required List<dynamic> devicesTokens,
-    required String body,
-    required String title,
-  }) async {
-    try {
-      for (final token in devicesTokens) {
-        try {
-          await http.post(
-            Uri.parse('https://fcm.googleapis.com/fcm/send'),
-            headers: <String, String>{
-              'Content-Type': 'application/json',
+  static Future<void> pushNotification(
+      {required CustomNotification customNotification,
+      required String token}) async {
+    String notificationRoute;
+    String routeParameterId;
+    if (customNotification.isThatPost) {
+      notificationRoute = "post";
+      routeParameterId = customNotification.postId;
+    } else {
+      notificationRoute = "profile";
+      routeParameterId = customNotification.senderId;
+    }
+    PushNotification detail = PushNotification(
+      title: customNotification.senderName,
+      body: customNotification.text,
+      deviceToken: token,
+      notificationRoute: notificationRoute,
+      routeParameterId: routeParameterId,
+    );
+    return await sendPopupNotification(pushNotification: detail);
+  }
 
-              /// conect with cloud messaging and get server key from project settings here
-              /// replace the points with your key  "key=...." and set it in [notificationKey]
-              'Authorization': notificationKey,
-            },
-            body: jsonEncode(
-              <String, dynamic>{
-                'notification': <String, dynamic>{'body': body, 'title': title},
-                'priority': 'high',
-                'data': <String, dynamic>{
-                  'click_action': 'FLUTTER_NOTIFICATION_CLICK',
-                  'id': '1',
-                  'status': 'done'
-                },
-                "to": token,
-              },
-            ),
-          );
-        } catch (e, s) {
-          if (kDebugMode) print(s);
-        }
+  static Future<void> sendPopupNotification(
+      {required PushNotification pushNotification}) async {
+    try {
+      try {
+        await http.post(
+          Uri.parse('https://fcm.googleapis.com/fcm/send'),
+          headers: <String, String>{
+            'Content-Type': 'application/json',
+
+            /// conect with cloud messaging and get server key from project settings here
+            /// replace the points with your key  "key=...." and set it in [notificationKey]
+            'Authorization': notificationKey,
+          },
+          body: jsonEncode(pushNotification.toMap()),
+        );
+      } catch (e, s) {
+        if (kDebugMode) print(s);
       }
     } catch (e) {
-      return Future.error("error push notification");
+      return Future.error("Error with push notification");
     }
   }
 }
