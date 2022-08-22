fc2dde542f9fa325f1913e7e496429d9feba3627
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-22 16:48:12 +0200

    handling calling video bugs

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -13,6 +13,7 @@ import 'package:instagram/core/utility/constant.dart';
 import 'package:instagram/presentation/pages/register/login_page.dart';
 import 'package:instagram/presentation/widgets/belong_to/register_w/get_my_user_info.dart';
 import 'package:instagram/presentation/widgets/global/others/multi_bloc_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class MyApp extends StatefulWidget {
@@ -31,9 +32,10 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void initState() {
-
     myId = widget.sharePrefs.getString("myPersonalId");
     if (myId != null) myPersonalId = myId!;
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async => await onJoin());
 
     /// It's prefer to the here not in data_sources to avoid bugs when push notification.
     if (isThatMobile) {
@@ -44,15 +46,23 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
+  Future<void> onJoin() async {
+    await _handleCameraAndMic(Permission.camera);
+    await _handleCameraAndMic(Permission.microphone);
+  }
+
+  Future<void> _handleCameraAndMic(Permission permission) async =>
+      await permission.request();
+
   void requestPermission() async {
     FirebaseMessaging messaging = FirebaseMessaging.instance;
     await messaging.requestPermission(
       alert: true,
-      announcement: true,
+      announcement: false,
       badge: true,
-      carPlay: true,
+      carPlay: false,
       criticalAlert: true,
-      provisional: true,
+      provisional: false,
       sound: true,
     );
   }
@@ -66,16 +76,27 @@ class _MyAppState extends State<MyApp> {
           notification.hashCode,
           notification.title,
           notification.body,
-          NotificationDetails(
-            android: AndroidNotificationDetails(
-              channel.id,
-              channel.name,
-              icon: 'launch_background',
-            ),
-          ),
+          payload: notification.body,
+          NotificationDetails(android: androidNotificationDetails()),
         );
       }
     });
+  }
+
+  AndroidNotificationDetails androidNotificationDetails() {
+    return AndroidNotificationDetails(
+      channel.id,
+      channel.name,
+      icon: 'launch_background',
+      importance: Importance.max,
+      enableLights: true,
+      playSound: true,
+      color: const Color.fromARGB(255, 0, 0, 0),
+      priority: Priority.high,
+      showWhen: true,
+      setAsGroupSummary: true,
+      sound: const RawResourceAndroidNotificationSound('notification'),
+    );
   }
 
   void loadFCM() async {
@@ -84,6 +105,9 @@ class _MyAppState extends State<MyApp> {
       'High Importance Notifications',
       importance: Importance.high,
       enableVibration: true,
+      playSound: true,
+      showBadge: true,
+      sound: RawResourceAndroidNotificationSound('notification'),
     );
 
     flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
@@ -110,29 +134,28 @@ class _MyAppState extends State<MyApp> {
 
   Widget materialApp(BuildContext context) {
     return GetBuilder<AppLanguage>(
-      init: AppLanguage(),
-      builder: (controller) {
-        return GetMaterialApp(
-          translations: Translation(),
-          locale: Locale(controller.appLocale),
-          fallbackLocale: const Locale('en'),
-          navigatorKey: navigatorKey,
-          debugShowCheckedModeBanner: false,
-          title: 'Instagram',
-          theme: AppTheme.light,
-          darkTheme: AppTheme.dark,
-          themeMode: ThemeOfApp().theme,
-          home: AnimatedSplashScreen(
-            centered: true,
-            splash: IconsAssets.splashIcon,
-            backgroundColor: ColorManager.white,
-            splashTransition: SplashTransition.scaleTransition,
-            nextScreen: myId == null
-                ? LoginPage(sharePrefs: widget.sharePrefs)
-                : GetMyPersonalInfo(myPersonalId: myId!),
-          ),
-        );
-      }
-    );
+        init: AppLanguage(),
+        builder: (controller) {
+          return GetMaterialApp(
+            translations: Translation(),
+            locale: Locale(controller.appLocale),
+            fallbackLocale: const Locale('en'),
+            navigatorKey: navigatorKey,
+            debugShowCheckedModeBanner: false,
+            title: 'Instagram',
+            theme: AppTheme.light,
+            darkTheme: AppTheme.dark,
+            themeMode: ThemeOfApp().theme,
+            home: AnimatedSplashScreen(
+              centered: true,
+              splash: IconsAssets.splashIcon,
+              backgroundColor: ColorManager.white,
+              splashTransition: SplashTransition.scaleTransition,
+              nextScreen: myId == null
+                  ? LoginPage(sharePrefs: widget.sharePrefs)
+                  : GetMyPersonalInfo(myPersonalId: myId!),
+            ),
+          );
+        });
   }
 }
