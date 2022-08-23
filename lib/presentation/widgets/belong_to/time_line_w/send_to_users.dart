43b5411cbb10eb6736b653ee8865706597d5c06e
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-24 00:01:50 +0200

    handling the depreciation of text theme

--- lib/presentation/widgets/belong_to/time_line_w/send_to_users.dart
+++ lib/presentation/widgets/belong_to/time_line_w/send_to_users.dart
@@ -147,11 +147,11 @@ class _SendToUsersState extends State<SendToUsers> {
             children: [
               Text(
                 userInfo.name,
-                style: Theme.of(context).textTheme.bodyText1,
+                style: Theme.of(context).textTheme.bodyLarge,
               ),
               Text(
                 userInfo.userName,
-                style: Theme.of(context).textTheme.headline1,
+                style: Theme.of(context).textTheme.displayLarge,
               ),
             ],
           ),
