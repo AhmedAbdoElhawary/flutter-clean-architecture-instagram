33d46184df9a4dd42817169db1295d0120fc1319
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-23 20:06:16 +0200

    handle calling video

--- lib/data/models/push_notification.dart
+++ lib/data/models/push_notification.dart
@@ -1,19 +1,18 @@
-
-
 class PushNotification {
   String body;
   String title;
   String deviceToken;
   String routeParameterId;
   String notificationRoute;
+  String userCallingId;
   PushNotification({
     required this.body,
     required this.title,
     required this.deviceToken,
+    this.userCallingId = '',
     required this.routeParameterId,
     required this.notificationRoute,
   });
-
 
   Map<String, dynamic> toMap() => {
         'notification': <String, dynamic>{'body': body, 'title': title},
@@ -22,6 +21,7 @@ class PushNotification {
           'click_action': 'FLUTTER_NOTIFICATION_CLICK',
           'route': notificationRoute,
           'routeParameterId': routeParameterId,
+          'userCallingId': userCallingId,
         },
         "to": deviceToken,
       };
