7e446dd3885dfb8a3101530e136d90f913958ec1
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-24 00:00:44 +0200

    handling the depreciation of text theme

--- lib/presentation/pages/activity/activity_for_mobile.dart
+++ lib/presentation/pages/activity/activity_for_mobile.dart
@@ -171,7 +171,7 @@ class _ShowNotificationsState extends State<_ShowNotifications> {
       return Center(
         child: Text(
           StringsManager.noActivity.tr,
-          style: Theme.of(context).textTheme.bodyText1,
+          style: Theme.of(context).textTheme.bodyLarge,
         ),
       );
     }
