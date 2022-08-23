5df74f275c878f9a13d40142626565f30468443f
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-23 20:06:12 +0200

    handle calling video

--- lib/core/functions/notifications_permissions.dart
+++ lib/core/functions/notifications_permissions.dart
@@ -1,3 +1,4 @@
+import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,14 +7,19 @@ import 'package:get/get.dart';
 import 'package:instagram/config/routes/app_routes.dart';
 import 'package:instagram/core/resources/strings_manager.dart';
 import 'package:instagram/core/utility/injector.dart';
+import 'package:instagram/data/models/user_personal_info.dart';
+import 'package:instagram/presentation/cubit/callingRooms/calling_rooms_cubit.dart';
 import 'package:instagram/presentation/cubit/firestoreUserInfoCubit/message/bloc/message_bloc.dart';
+import 'package:instagram/presentation/cubit/firestoreUserInfoCubit/user_info_cubit.dart';
 import 'package:instagram/presentation/pages/messages/chatting_page.dart';
+import 'package:instagram/presentation/pages/messages/video_call_page.dart';
 import 'package:instagram/presentation/widgets/belong_to/profile_w/which_profile_page.dart';
 import 'package:instagram/presentation/widgets/global/custom_widgets/get_post_info.dart';
 
-late FlutterLocalNotificationsPlugin _localNotifications;
-late AndroidNotificationChannel _channel;
-
+late FlutterLocalNotificationsPlugin _normalLocalNotifications;
+late AndroidNotificationChannel _normalChannel;
+late FlutterLocalNotificationsPlugin _videoCallLocalNotifications;
+late AndroidNotificationChannel _videoCallChannel;
 Future<void> notificationPermissions(BuildContext context) async {
   await _requestPermission();
   await _listenFCM(context);
@@ -35,13 +41,32 @@ Future<void> _listenFCM(BuildContext context) async {
     RemoteNotification? notification = message.notification;
     AndroidNotification? android = message.notification?.android;
     if (notification != null && android != null) {
-      _localNotifications.show(
-        notification.hashCode,
-        notification.title,
-        notification.body,
-        payload: "${message.data["route"]},${message.data["routeParameterId"]}",
-        NotificationDetails(android: _androidNotificationDetails()),
-      );
+      dynamic route = message.data["route"];
+      dynamic routeParameterId = message.data["routeParameterId"];
+      dynamic userCallingId = message.data["userCallingId"];
+
+      bool isThatVideoCall = route == "call";
+      if (isThatVideoCall) {
+        _videoCallLocalNotifications.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          payload: "$route,$routeParameterId,$userCallingId",
+          NotificationDetails(
+              android: _videoCallAndroidNotificationDetails(
+                  channel: _videoCallChannel, isThatCalling: true)),
+        );
+      } else {
+        _normalLocalNotifications.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          payload: "$route,${message.data["routeParameterId"]}",
+          NotificationDetails(
+              android: _videoCallAndroidNotificationDetails(
+                  channel: _normalChannel)),
+        );
+      }
     }
   });
 
@@ -59,11 +84,14 @@ Future<void> _handleMessage(BuildContext context, RemoteMessage message) async {
   await _pushToPage(
       route: message.data["route"],
       routeParameterId: message.data["routeParameterId"],
+      userCallingId: message.data["userCallingId"],
       context);
 }
 
 Future<void> _pushToPage(BuildContext context,
-    {required dynamic route, required dynamic routeParameterId}) async {
+    {required dynamic route,
+    required dynamic routeParameterId,
+    required dynamic userCallingId}) async {
   Widget page;
   if (route == "post") {
     page = GetsPostInfoAndDisplay(
@@ -72,6 +100,16 @@ Future<void> _pushToPage(BuildContext context,
     );
   } else if (route == "profile") {
     page = WhichProfilePage(userId: routeParameterId);
+  } else if (route == "call") {
+    UserPersonalInfo myPersonalInfo = UserInfoCubit.getMyPersonalInfo(context);
+    await CallingRoomsCubit.get(context)
+        .joinToRoom(channelId: routeParameterId, userInfo: myPersonalInfo);
+    page = CallPage(
+      channelName: routeParameterId,
+      role: ClientRole.Broadcaster,
+      userCallingId: userCallingId,
+      userCallingType: UserCallingType.receiver,
+    );
   } else {
     page = BlocProvider<MessageBloc>(
       create: (context) => injector<MessageBloc>(),
@@ -81,10 +119,11 @@ Future<void> _pushToPage(BuildContext context,
   await pushToPage(context, page: page);
 }
 
-AndroidNotificationDetails _androidNotificationDetails() {
+AndroidNotificationDetails _videoCallAndroidNotificationDetails(
+    {required AndroidNotificationChannel channel, bool isThatCalling = false}) {
   return AndroidNotificationDetails(
-    _channel.id,
-    _channel.name,
+    channel.id,
+    channel.name,
     icon: 'launch_background',
     importance: Importance.max,
     enableLights: true,
@@ -92,27 +131,31 @@ AndroidNotificationDetails _androidNotificationDetails() {
     priority: Priority.high,
     showWhen: true,
     setAsGroupSummary: true,
-    sound: const RawResourceAndroidNotificationSound('notification'),
+    timeoutAfter: isThatCalling ? 75000 : null,
+    fullScreenIntent: isThatCalling,
+    sound: isThatCalling
+        ? const RawResourceAndroidNotificationSound('video')
+        : const RawResourceAndroidNotificationSound('notification'),
   );
 }
 
 Future<void> _loadFCM(BuildContext context) async {
-  final int id = DateTime.now().microsecondsSinceEpoch ~/ 1000;
-  _channel = AndroidNotificationChannel(
-    id.toString(),
-    'high_importance_channel',
-    importance: Importance.max,
-    enableVibration: true,
-    playSound: true,
-    showBadge: true,
-    sound: const RawResourceAndroidNotificationSound('notification'),
-  );
+  _normalChannel = _androidNotificationChannel();
+  _videoCallChannel = _androidNotificationChannel(isThatCalling: true);
 
-  _localNotifications = FlutterLocalNotificationsPlugin();
-  await _localNotifications
+  _normalLocalNotifications = FlutterLocalNotificationsPlugin();
+  _videoCallLocalNotifications = FlutterLocalNotificationsPlugin();
+
+  await _normalLocalNotifications
       .resolvePlatformSpecificImplementation<
           AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(_channel);
+      ?.createNotificationChannel(_normalChannel);
+
+  await _videoCallLocalNotifications
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(_videoCallChannel);
+
   await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
     alert: true,
     badge: true,
@@ -122,14 +165,42 @@ Future<void> _loadFCM(BuildContext context) async {
       android: AndroidInitializationSettings("launch_background"));
 
   /// when app opened and select the message
-  _localNotifications.initialize(initializationSettings,
-      onSelectNotification: (String? payload) async {
-    if (payload != null) {
-      List<String> s = payload.split(",");
-      String route = s[0];
-      String routeParameterId = s[1];
-      await _pushToPage(
-          route: route, routeParameterId: routeParameterId, context);
-    }
-  });
+  _normalLocalNotifications.initialize(initializationSettings,
+      onSelectNotification: (p) => _onSelectNotification(context, p));
+
+  _videoCallLocalNotifications.initialize(initializationSettings,
+      onSelectNotification: (p) => _onSelectNotification(context, p));
+}
+
+_onSelectNotification(BuildContext context, String? payload) async {
+  if (payload != null) {
+    List<String> s = payload.split(",");
+    String route = s[0];
+    String routeParameterId = s[1];
+    String userCallingId = s[2];
+
+    await _pushToPage(
+      context,
+      route: route,
+      routeParameterId: routeParameterId,
+      userCallingId: userCallingId,
+    );
+  }
+}
+
+AndroidNotificationChannel _androidNotificationChannel(
+    {bool isThatCalling = false}) {
+  final int id = DateTime.now().microsecondsSinceEpoch ~/ 1000;
+
+  return AndroidNotificationChannel(
+    id.toString(),
+    'high_importance_channel',
+    importance: Importance.max,
+    enableVibration: true,
+    playSound: true,
+    showBadge: true,
+    sound: isThatCalling
+        ? const RawResourceAndroidNotificationSound('video')
+        : const RawResourceAndroidNotificationSound('notification'),
+  );
 }
