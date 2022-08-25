c737d1e9f73be08034d6e03ae8ef9edc9a8adefc
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-25 22:56:53 +0200

    reformat the code

--- lib/presentation/cubit/firestoreUserInfoCubit/message/cubit/message_cubit.dart
+++ lib/presentation/cubit/firestoreUserInfoCubit/message/cubit/message_cubit.dart
@@ -1,28 +1,29 @@
 import 'dart:io';
 import 'dart:typed_data';
-
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:instagram/data/models/message.dart';
-import 'package:instagram/domain/use_cases/user/message/add_message.dart';
-import 'package:instagram/domain/use_cases/user/message/delete_message.dart';
+import 'package:instagram/data/models/parent_classes/without_sub_classes/single_message.dart';
+import 'package:instagram/domain/use_cases/message/single_message/add_message.dart';
+import 'package:instagram/domain/use_cases/message/single_message/delete_message.dart';
 
 part 'message_state.dart';
 
 class MessageCubit extends Cubit<MessageState> {
   final AddMessageUseCase _addMessageUseCase;
   final DeleteMessageUseCase _deleteMessageUseCase;
+
   List<Message> messagesInfo = [];
   MessageCubit(this._addMessageUseCase, this._deleteMessageUseCase)
       : super(MessageInitial());
 
   static MessageCubit get(BuildContext context) => BlocProvider.of(context);
 
-  Future<void> sendMessage(
-      {required Message messageInfo,
-      Uint8List? pathOfPhoto,
-      File? recordFile}) async {
+  Future<void> sendMessage({
+    required Message messageInfo,
+    Uint8List? pathOfPhoto,
+    File? recordFile,
+  }) async {
     emit(SendMessageLoading());
     await _addMessageUseCase
         .call(
@@ -36,11 +37,12 @@ class MessageCubit extends Cubit<MessageState> {
     });
   }
 
+
   Future<void> deleteMessage(
       {required Message messageInfo, Message? replacedMessage}) async {
     emit(DeleteMessageLoading());
     await _deleteMessageUseCase
-        .call(paramsOne: messageInfo, paramsTwo: replacedMessage!)
+        .call(paramsOne: messageInfo, paramsTwo: replacedMessage)
         .then((_) {
       emit(DeleteMessageLoaded());
     }).catchError((e) {
