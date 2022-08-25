a9fb35d7ac47785b9a18349dc8dcbfec0939ab77
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-25 22:54:39 +0200

    create GetMessagesGroGroupChatUseCase

---
+++ lib/domain/use_cases/message/group_message/get_messages.dart
@@ -0,0 +1,15 @@
+import 'package:instagram/core/use_case/use_case.dart';
+import 'package:instagram/data/models/parent_classes/without_sub_classes/single_message.dart';
+import 'package:instagram/domain/repositories/group_message.dart';
+
+class GetMessagesGroGroupChatUseCase
+    implements StreamUseCase<List<Message>, String> {
+  final FirestoreGroupMessageRepository _addPostToUserRepository;
+
+  GetMessagesGroGroupChatUseCase(this._addPostToUserRepository);
+
+  @override
+  Stream<List<Message>> call({required String params}) {
+    return _addPostToUserRepository.getMessages(groupChatUid: params);
+  }
+}
