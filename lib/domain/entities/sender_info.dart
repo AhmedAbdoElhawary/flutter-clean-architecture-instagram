e39f358ca459b425b410de0f7c1de3245e0e1702
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-25 22:52:57 +0200

    rename some files

---
+++ lib/domain/entities/sender_info.dart
@@ -0,0 +1,15 @@
+import 'package:instagram/data/models/parent_classes/without_sub_classes/single_message.dart';
+import 'package:instagram/data/models/parent_classes/without_sub_classes/user_personal_info.dart';
+
+/// [SenderInfo] it can be sender or receiver
+class SenderInfo {
+  List<UserPersonalInfo>? receiversInfo;
+
+  Message? lastMessage;
+  bool isThatGroupChat;
+  SenderInfo({
+    this.receiversInfo,
+    this.lastMessage,
+    this.isThatGroupChat = false,
+  });
+}
