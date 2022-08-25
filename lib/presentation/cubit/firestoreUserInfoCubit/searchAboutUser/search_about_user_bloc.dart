0287bde77787827498541ba682ef1ca9f6670eb3
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-25 22:52:52 +0200

    rename some files

--- lib/presentation/cubit/firestoreUserInfoCubit/searchAboutUser/search_about_user_bloc.dart
+++ lib/presentation/cubit/firestoreUserInfoCubit/searchAboutUser/search_about_user_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:instagram/data/models/user_personal_info.dart';
+import 'package:instagram/data/models/parent_classes/without_sub_classes/user_personal_info.dart';
 import 'package:instagram/domain/use_cases/user/search_about_user.dart';
 
 part 'search_about_user_event.dart';
