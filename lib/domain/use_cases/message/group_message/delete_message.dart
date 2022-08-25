da0415f3b56d220671ed06aa8065e8c1eb0a92a6
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-25 22:54:34 +0200

    create DeleteMessageForGroupChatUseCase

---
+++ lib/domain/use_cases/message/group_message/delete_message.dart
@@ -0,0 +1,22 @@
+import 'package:instagram/core/use_case/use_case.dart';
+import 'package:instagram/data/models/parent_classes/without_sub_classes/single_message.dart';
+import 'package:instagram/domain/repositories/group_message.dart';
+
+class DeleteMessageForGroupChatUseCase
+    implements UseCaseThreeParams<void, String, Message, Message?> {
+  final FirestoreGroupMessageRepository _addPostToUserRepository;
+
+  DeleteMessageForGroupChatUseCase(this._addPostToUserRepository);
+
+  @override
+  Future<void> call({
+    required String paramsOne,
+    required Message paramsTwo,
+    required Message? paramsThree,
+  }) {
+    return _addPostToUserRepository.deleteMessage(
+        chatOfGroupUid: paramsOne,
+        messageInfo: paramsTwo,
+        replacedMessage: paramsThree);
+  }
+}
