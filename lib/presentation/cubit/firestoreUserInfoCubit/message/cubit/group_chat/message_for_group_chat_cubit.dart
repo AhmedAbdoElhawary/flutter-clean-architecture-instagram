e78049427ec705d888fd956a879f64e7cc323b2c
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-26 01:38:45 +0200

    change the file name single_message to message

--- lib/presentation/cubit/firestoreUserInfoCubit/message/cubit/group_chat/message_for_group_chat_cubit.dart
+++ lib/presentation/cubit/firestoreUserInfoCubit/message/cubit/group_chat/message_for_group_chat_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:instagram/data/models/parent_classes/without_sub_classes/single_message.dart';
+import 'package:instagram/data/models/parent_classes/without_sub_classes/message.dart';
 import 'package:instagram/domain/use_cases/message/group_message/add_message.dart';
 import 'dart:io';
 import 'dart:typed_data';
