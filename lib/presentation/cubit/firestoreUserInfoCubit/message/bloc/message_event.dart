61cd927ddcd800d2be8e8b400b82e5a2f51a9a50
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-25 22:57:12 +0200

    add LoadMessagesForGroupChat,UpdateMessagesForGroup in MessageEvent

--- lib/presentation/cubit/firestoreUserInfoCubit/message/bloc/message_event.dart
+++ lib/presentation/cubit/firestoreUserInfoCubit/message/bloc/message_event.dart
@@ -7,12 +7,27 @@ abstract class MessageEvent extends Equatable {
   List<Object> get props => [];
 }
 
-class LoadMessages extends MessageEvent {
+class LoadMessagesForSingleChat extends MessageEvent {
   final String receiverId;
 
-  const LoadMessages(this.receiverId);
+  const LoadMessagesForSingleChat(this.receiverId);
   @override
   List<Object> get props => [receiverId];
+}
+
+class LoadMessagesForGroupChat extends MessageEvent {
+  final String groupChatUid;
+
+  const LoadMessagesForGroupChat({required this.groupChatUid});
+  @override
+  List<Object> get props => [groupChatUid];
+}
+class UpdateMessagesForGroup extends MessageEvent {
+  final List<Message> messages;
+
+  const UpdateMessagesForGroup(this.messages);
+  @override
+  List<Object> get props => [messages];
 }
 
 class UpdateMessages extends MessageEvent {
