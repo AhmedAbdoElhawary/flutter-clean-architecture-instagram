ea095e46089c5c843d9d3fa9a3fd9d72504fda02
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-25 22:48:12 +0200

    rename some files

--- lib/domain/use_cases/user/add_post_to_user.dart
+++ lib/domain/use_cases/user/add_post_to_user.dart
@@ -1,6 +1,6 @@
 import 'package:instagram/core/use_case/use_case.dart';
-import 'package:instagram/data/models/post.dart';
-import '../../../data/models/user_personal_info.dart';
+import 'package:instagram/data/models/child_classes/post/post.dart';
+import '../../../data/models/parent_classes/without_sub_classes/user_personal_info.dart';
 import '../../repositories/user_repository.dart';
 
 class AddPostToUserUseCase
