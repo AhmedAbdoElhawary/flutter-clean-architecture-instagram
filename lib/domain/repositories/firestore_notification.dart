4523ead5aea00c71443c1d2c650a3769c08f8ccc
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-25 22:49:52 +0200

    rename some files

--- lib/domain/repositories/firestore_notification.dart
+++ lib/domain/repositories/firestore_notification.dart
@@ -1,4 +1,4 @@
-import 'package:instagram/data/models/notification.dart';
+import 'package:instagram/data/models/child_classes/child_classes_with_entities/notification.dart';
 import 'package:instagram/domain/entities/notification_check.dart';
 
 abstract class FirestoreNotificationRepository {
