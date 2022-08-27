ee76bb0fc08944f9a8089da59faa25d50e774b13
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-27 21:03:03 +0200

    change the name of some parameters

--- lib/domain/use_cases/calling_rooms/get_users_info_in_room.dart
+++ lib/domain/use_cases/calling_rooms/get_users_info_in_room.dart
@@ -3,11 +3,11 @@ import 'package:instagram/domain/repositories/calling_rooms_repository.dart';
 import 'package:instagram/core/use_case/use_case.dart';
 
 class GetUsersInfoInRoomUseCase
-    extends UseCase<List<UserInfoInCallingRoom>, String> {
+    extends UseCase<List<UsersInfoInCallingRoom>, String> {
   final CallingRoomsRepository _callingRoomsRepo;
   GetUsersInfoInRoomUseCase(this._callingRoomsRepo);
   @override
-  Future<List<UserInfoInCallingRoom>> call({required String params}) {
+  Future<List<UsersInfoInCallingRoom>> call({required String params}) {
     return _callingRoomsRepo.getUsersInfoInThisRoom(channelId: params);
   }
 }
