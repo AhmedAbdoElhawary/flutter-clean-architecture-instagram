103160b8268acbf60997428d88d99c75dcc9377b
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-27 23:06:47 +0200

    refactoring the code

--- lib/presentation/widgets/global/others/notification_card_info.dart
+++ lib/presentation/widgets/global/others/notification_card_info.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:get/get.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -10,34 +11,63 @@ import 'package:instagram/core/resources/styles_manager.dart';
 import 'package:instagram/core/utility/constant.dart';
 import 'package:instagram/data/models/child_classes/child_classes_with_entities/notification.dart';
 import 'package:instagram/presentation/widgets/belong_to/profile_w/which_profile_page.dart';
-import 'package:instagram/presentation/widgets/global/custom_widgets/custom_network_image_display.dart';
 import 'package:instagram/presentation/widgets/global/custom_widgets/get_post_info.dart';
 
-class NotificationCardInfo extends StatelessWidget {
+class NotificationCardInfo extends StatefulWidget {
   final CustomNotification notificationInfo;
   const NotificationCardInfo({Key? key, required this.notificationInfo})
       : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    String reformatDate = DateOfNow.commentsDateOfNow(notificationInfo.time);
-    List<String> hashOfUserName = hashUserName(notificationInfo.text);
+  State<NotificationCardInfo> createState() => _NotificationCardInfoState();
+}
 
+class _NotificationCardInfoState extends State<NotificationCardInfo> {
+  late String profileImage;
+
+  @override
+  void initState() {
+    profileImage = widget.notificationInfo.personalProfileImageUrl;
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (profileImage.isNotEmpty) {
+      precacheImage(NetworkImage(profileImage), context);
+    }
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String reformatDate =
+        DateOfNow.commentsDateOfNow(widget.notificationInfo.time);
+    List<String> hashOfUserName = hashUserName(widget.notificationInfo.text);
     return InkWell(
       onTap: () async {
         await pushToPage(context,
-            page: WhichProfilePage(userId: notificationInfo.senderId),
+            page: WhichProfilePage(userId: widget.notificationInfo.senderId),
             withoutRoot: false);
       },
       child: Padding(
         padding: EdgeInsetsDirectional.only(
             start: 15, top: 15, end: 15, bottom: !isThatMobile ? 5 : 0),
         child: Row(children: [
-          NetworkImageDisplay(
-            imageUrl: notificationInfo.personalProfileImageUrl,
-            height: 300,
-            cachingHeight: 300,
-            cachingWidth: 300,
+          CircleAvatar(
+            backgroundColor: ColorManager.customGrey,
+            backgroundImage: profileImage.isNotEmpty
+                ? CachedNetworkImageProvider(profileImage,
+                    maxWidth: 165, maxHeight: 165)
+                : null,
+            radius: 25,
+            child: profileImage.isEmpty
+                ? Icon(
+                    Icons.person,
+                    color: Theme.of(context).primaryColor,
+                    size: 25,
+                  )
+                : null,
           ),
           const SizedBox(width: 10),
           Expanded(
@@ -45,7 +75,7 @@ class NotificationCardInfo extends StatelessWidget {
               TextSpan(
                 children: <TextSpan>[
                   TextSpan(
-                    text: "${notificationInfo.personalUserName} ",
+                    text: "${widget.notificationInfo.personalUserName} ",
                     style: Theme.of(context).textTheme.displayMedium,
                   ),
                   if (hashOfUserName.isNotEmpty) ...[
@@ -70,7 +100,7 @@ class NotificationCardInfo extends StatelessWidget {
                     ),
                   ] else ...[
                     TextSpan(
-                      text: "${notificationInfo.text} ",
+                      text: "${widget.notificationInfo.text} ",
                       style: Theme.of(context).textTheme.bodyLarge,
                     ),
                   ],
@@ -83,13 +113,13 @@ class NotificationCardInfo extends StatelessWidget {
             ),
           ),
           const SizedBox(width: 10),
-          if (notificationInfo.isThatPost &&
-              notificationInfo.postImageUrl.isNotEmpty)
+          if (widget.notificationInfo.isThatPost &&
+              widget.notificationInfo.postImageUrl.isNotEmpty)
             GestureDetector(
               onTap: () {
                 String appBarText;
-                if (notificationInfo.isThatPost &&
-                    notificationInfo.isThatLike) {
+                if (widget.notificationInfo.isThatPost &&
+                    widget.notificationInfo.isThatLike) {
                   appBarText = StringsManager.post.tr;
                 } else {
                   appBarText = StringsManager.comments.tr;
@@ -97,21 +127,21 @@ class NotificationCardInfo extends StatelessWidget {
                 if (isThatMobile) {
                   pushToPage(context,
                       page: GetsPostInfoAndDisplay(
-                        postId: notificationInfo.postId,
+                        postId: widget.notificationInfo.postId,
                         appBarText: appBarText,
                       ),
                       withoutRoot: false);
                 } else {
                   Navigator.of(context).push(HeroDialogRoute(
                     builder: (context) => GetsPostInfoAndDisplay(
-                      postId: notificationInfo.postId,
+                      postId: widget.notificationInfo.postId,
                       appBarText: appBarText,
                       fromHeroRoute: true,
                     ),
                   ));
                 }
               },
-              child: Image.network(notificationInfo.postImageUrl,
+              child: Image.network(widget.notificationInfo.postImageUrl,
                   height: 45, width: 45),
             ),
         ]),
