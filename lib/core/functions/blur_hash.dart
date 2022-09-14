0c5355cf3b9ada6c259b01ff545605e6f363490f
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-09-14 19:37:13 +0200

    put blurHashEncode in CustomBlurHash

--- lib/core/functions/blur_hash.dart
+++ lib/core/functions/blur_hash.dart
@@ -1,7 +1,9 @@
 import 'dart:typed_data';
 import 'package:blurhash/blurhash.dart';
 
-Future<String> blurHashEncode(Uint8List pixels) async {
-  String result = await BlurHash.encode(pixels, 4, 3);
-  return result;
+class CustomBlurHash {
+  static Future<String> blurHashEncode(Uint8List pixels) async {
+    String result = await BlurHash.encode(pixels, 4, 3);
+    return result;
+  }
 }
