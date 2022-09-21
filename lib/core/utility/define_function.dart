7653aaa287777556de391a5b8e7e32075a2ffc31
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-09-21 23:54:08 +0200

    delete unuseful file

--- lib/core/utility/define_function.dart
+++
@@ -1,5 +0,0 @@
-import 'package:instagram/data/models/parent_classes/without_sub_classes/comment.dart';
-
-typedef SelectedCommentCallback = void Function(Comment commentInfo, int index);
-typedef UpdateFollowersCallback = void Function(
-    bool isThatAdding, dynamic userId);
