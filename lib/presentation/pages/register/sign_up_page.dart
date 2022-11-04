38ef9324c3354252a4ab023cfa4c9d21ef57710d
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-11-04 18:49:00 +0200

    restructure SignUpPage

--- lib/presentation/pages/register/sign_up_page.dart
+++ lib/presentation/pages/register/sign_up_page.dart
@@ -1,6 +1,6 @@
-import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
 import 'package:instagram/config/routes/app_routes.dart';
 import 'package:instagram/core/resources/color_manager.dart';
 import 'package:instagram/core/resources/strings_manager.dart';
@@ -9,17 +9,15 @@ import 'package:instagram/core/utility/constant.dart';
 import 'package:instagram/core/utility/injector.dart';
 import 'package:instagram/domain/entities/registered_user.dart';
 import 'package:instagram/presentation/cubit/firestoreUserInfoCubit/searchAboutUser/search_about_user_bloc.dart';
-import 'package:instagram/presentation/cubit/firestoreUserInfoCubit/user_info_cubit.dart';
-import 'package:instagram/presentation/screens/responsive_layout.dart';
-import 'package:instagram/presentation/screens/web_screen_layout.dart';
-import 'package:instagram/presentation/widgets/belong_to/register_w/popup_calling.dart';
+import 'package:instagram/presentation/widgets/belong_to/register_w/get_my_user_info.dart';
 import 'package:instagram/presentation/widgets/belong_to/register_w/register_widgets.dart';
 import 'package:instagram/presentation/widgets/global/custom_widgets/custom_elevated_button.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../core/functions/toast_show.dart';
 import '../../../data/models/parent_classes/without_sub_classes/user_personal_info.dart';
 import '../../cubit/firebaseAuthCubit/firebase_auth_cubit.dart';
 import '../../cubit/firestoreUserInfoCubit/add_new_user_cubit.dart';
-import '../../../core/functions/toast_show.dart';
 
 class TextsControllers {
   final TextEditingController emailController;
@@ -102,26 +100,18 @@ class _SignUpPageState extends State<SignUpPage> {
   }
 }
 
-class UserNamePage extends StatefulWidget {
+class UserNamePage extends StatelessWidget {
   final TextsControllers textsControllers;
-  const UserNamePage(this.textsControllers, {Key? key}) : super(key: key);
+  UserNamePage(this.textsControllers, {Key? key}) : super(key: key);
 
-  @override
-  State<UserNamePage> createState() => _UserNamePageState();
-}
-
-class _UserNamePageState extends State<UserNamePage> {
   final userNameController = TextEditingController();
-  final isToastShowed = ValueNotifier(false);
-  bool validateEdits = false;
-  bool isFieldEmpty = true;
-  bool isItMoved = false;
 
-  @override
-  dispose() {
-    super.dispose();
-    isToastShowed.dispose();
-  }
+  final RxBool isToastShowed = false.obs;
+
+  final RxBool validateEdits = false.obs;
+  final RxBool isFieldEmpty = true.obs;
+
+  final RxBool isHeMovedToHome = false.obs;
 
   @override
   Widget build(BuildContext context) {
@@ -188,63 +178,61 @@ class _UserNamePageState extends State<UserNamePage> {
     );
   }
 
-  BlocBuilder<SearchAboutUserBloc, SearchAboutUserState> userNameTextField(
-      BuildContext context) {
-    return BlocBuilder<SearchAboutUserBloc, SearchAboutUserState>(
+  Widget userNameTextField(BuildContext context) {
+    return BlocListener<SearchAboutUserBloc, SearchAboutUserState>(
       bloc: BlocProvider.of<SearchAboutUserBloc>(context)
         ..add(FindSpecificUser(userNameController.text,
             searchForSingleLetter: true)),
-      buildWhen: (previous, current) =>
-          previous != current && (current is SearchAboutUserBlocLoaded),
-      builder: (context, state) {
+      listener: (context, state) {
         List<UserPersonalInfo> usersWithSameUserName = [];
         if (state is SearchAboutUserBlocLoaded) {
           usersWithSameUserName = state.users;
         }
-        WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {
-              validateEdits = usersWithSameUserName.isEmpty;
-              if (userNameController.text.isEmpty) {
-                validateEdits = false;
-                isFieldEmpty = true;
-              } else {
-                isFieldEmpty = false;
-              }
-            }));
-
-        return customTextField(validateEdits);
+        validateEdits.value = usersWithSameUserName.isEmpty;
+        if (userNameController.text.isEmpty) {
+          validateEdits.value = false;
+          isFieldEmpty.value = true;
+        } else {
+          isFieldEmpty.value = false;
+        }
       },
+      child: customTextField(context),
+      listenWhen: (previous, current) =>
+          previous != current && (current is SearchAboutUserBlocLoaded),
     );
   }
 
-  Padding customTextField(bool uniqueUserName) {
+  Padding customTextField(BuildContext context) {
     return Padding(
       padding: const EdgeInsetsDirectional.only(start: 20, end: 20),
       child: SizedBox(
         height: isThatMobile ? null : 37,
         width: double.infinity,
-        child: TextFormField(
-          controller: userNameController,
-          cursorColor: ColorManager.teal,
-          style:
-              getNormalStyle(color: Theme.of(context).focusColor, fontSize: 15),
-          decoration: InputDecoration(
-            hintText: StringsManager.username.tr,
-            hintStyle: isThatMobile
-                ? getNormalStyle(color: Theme.of(context).indicatorColor)
-                : getNormalStyle(color: ColorManager.black54, fontSize: 12),
-            fillColor: const Color.fromARGB(48, 232, 232, 232),
-            filled: true,
-            focusedBorder: outlineInputBorder(),
-            suffixIcon: isFieldEmpty
-                ? null
-                : (uniqueUserName ? rightIcon() : wrongIcon()),
-            enabledBorder: outlineInputBorder(),
-            contentPadding: EdgeInsets.symmetric(
-                horizontal: 10, vertical: isThatMobile ? 15 : 5),
-            errorText: isFieldEmpty || uniqueUserName
-                ? null
-                : StringsManager.thisUserNameExist.tr,
-            errorStyle: getNormalStyle(color: ColorManager.red),
+        child: Obx(
+          () => TextFormField(
+            controller: userNameController,
+            cursorColor: ColorManager.teal,
+            style: getNormalStyle(
+                color: Theme.of(context).focusColor, fontSize: 15),
+            decoration: InputDecoration(
+              hintText: StringsManager.username.tr,
+              hintStyle: isThatMobile
+                  ? getNormalStyle(color: Theme.of(context).indicatorColor)
+                  : getNormalStyle(color: ColorManager.black54, fontSize: 12),
+              fillColor: const Color.fromARGB(48, 232, 232, 232),
+              filled: true,
+              focusedBorder: outlineInputBorder(),
+              suffixIcon: isFieldEmpty.value
+                  ? null
+                  : (validateEdits.value ? rightIcon() : wrongIcon()),
+              enabledBorder: outlineInputBorder(),
+              contentPadding: EdgeInsets.symmetric(
+                  horizontal: 10, vertical: isThatMobile ? 15 : 5),
+              errorText: isFieldEmpty.value || validateEdits.value
+                  ? null
+                  : StringsManager.thisUserNameExist.tr,
+              errorStyle: getNormalStyle(color: ColorManager.red),
+            ),
           ),
         ),
       ),
@@ -275,28 +263,29 @@ class _UserNamePageState extends State<UserNamePage> {
     return Builder(builder: (context) {
       FireStoreAddNewUserCubit userCubit =
           FireStoreAddNewUserCubit.get(context);
-      return ValueListenableBuilder(
-        valueListenable: isToastShowed,
-        builder: (context, bool isToastShowedValue, child) =>
-            BlocBuilder<FirebaseAuthCubit, FirebaseAuthCubitState>(
-          builder: (context, authState) {
-            FirebaseAuthCubit authCubit = FirebaseAuthCubit.get(context);
-            if (authState is CubitAuthConfirmed) {
-              addNewUser(authState, userCubit);
-              moveToMain(authState);
-            } else if (authState is CubitAuthFailed && !isToastShowedValue) {
-              authFailed(authState);
+      return Obx(
+        () => BlocConsumer<FirebaseAuthCubit, FirebaseAuthCubitState>(
+          listener: (context, state) {
+            if (state is CubitAuthConfirmed) {
+              addNewUser(state, userCubit);
+              moveToMain(state);
+            } else if (state is CubitAuthFailed && !isToastShowed.value) {
+              authFailed(state);
             }
+          },
+          builder: (context, authState) {
             return CustomElevatedButton(
               isItDone: authState is! CubitAuthConfirming,
               nameOfButton: StringsManager.signUp.tr,
-              blueColor: validateEdits,
+              blueColor: validateEdits.value,
               onPressed: () async {
-                if (validateEdits) {
+                FirebaseAuthCubit authCubit = FirebaseAuthCubit.get(context);
+
+                if (validateEdits.value) {
                   isToastShowed.value = false;
                   await authCubit.signUp(RegisteredUser(
-                    email: widget.textsControllers.emailController.text,
-                    password: widget.textsControllers.passwordController.text,
+                    email: textsControllers.emailController.text,
+                    password: textsControllers.passwordController.text,
                   ));
                 }
               },
@@ -307,23 +296,19 @@ class _UserNamePageState extends State<UserNamePage> {
     });
   }
 
-  moveToMain(CubitAuthConfirmed authState) {
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      myPersonalId = authState.user.uid;
-      final SharedPreferences sharePrefs = injector<SharedPreferences>();
-      await sharePrefs.setString("myPersonalId", myPersonalId);
-      if (!mounted) return;
-      await UserInfoCubit.get(context).getUserInfo(myPersonalId);
-      if (!isItMoved) {
-        isItMoved = true;
-        Get.offAll(
-          ResponsiveLayout(
-            mobileScreenLayout: PopupCalling(myPersonalId),
-            webScreenLayout: const WebScreenLayout(),
-          ),
-        );
+  moveToMain(CubitAuthConfirmed authState) async {
+    myPersonalId = authState.user.uid;
+
+    final SharedPreferences sharePrefs = injector<SharedPreferences>();
+    if (!isHeMovedToHome.value) {
+      isHeMovedToHome.value = true;
+      if (myPersonalId.isNotEmpty) {
+        await sharePrefs.setString("myPersonalId", myPersonalId);
+        Get.offAll(GetMyPersonalInfo(myPersonalId: myPersonalId));
+      } else {
+        ToastShow.toast(StringsManager.somethingWrong.tr);
       }
-    });
+    }
   }
 
   authFailed(CubitAuthFailed authState) {
@@ -338,7 +323,7 @@ class _UserNamePageState extends State<UserNamePage> {
   }
 
   addNewUser(CubitAuthConfirmed authState, FireStoreAddNewUserCubit userCubit) {
-    String fullName = widget.textsControllers.fullNameController.text;
+    String fullName = textsControllers.fullNameController.text;
     List<dynamic> charactersOfName = [];
     String nameOfLower = fullName.toLowerCase();
 
