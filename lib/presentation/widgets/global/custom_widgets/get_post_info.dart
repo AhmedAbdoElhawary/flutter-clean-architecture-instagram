ab57a7f960576c9caafc40054bd86f29cb1e2e68
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-01-03 20:38:43 +0200

    replace CircularProgressIndicator to ThineCircularProgress

--- lib/presentation/widgets/global/custom_widgets/get_post_info.dart
+++ lib/presentation/widgets/global/custom_widgets/get_post_info.dart
@@ -4,6 +4,7 @@ import 'package:instagram/core/utility/constant.dart';
 import 'package:instagram/presentation/cubit/postInfoCubit/post_cubit.dart';
 import 'package:instagram/presentation/pages/comments/widgets/comment_of_post.dart';
 import 'package:instagram/presentation/widgets/global/custom_widgets/custom_app_bar.dart';
+import 'package:instagram/presentation/widgets/global/custom_widgets/custom_circulars_progress.dart';
 import 'package:instagram/presentation/widgets/global/custom_widgets/custom_posts_display.dart';
 import 'package:instagram/presentation/widgets/global/others/image_of_post.dart';
 
@@ -63,12 +64,10 @@ class GetsPostInfoAndDisplay extends StatelessWidget {
               );
             }
           } else {
-            return Center(
-              child: CircularProgressIndicator(
-                strokeWidth: 1.5,
-                color: Theme.of(context).iconTheme.color,
-                backgroundColor: Theme.of(context).dividerColor,
-              ),
+            return ThineCircularProgress(
+              strokeWidth: 1.5,
+              color: Theme.of(context).iconTheme.color,
+              backgroundColor: Theme.of(context).dividerColor,
             );
           }
         },
