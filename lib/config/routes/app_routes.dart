4db101131c3bd3bf0cec20889ccf047ca3033f35
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-01-25 22:07:29 +0200

    change pushToPage to Go.push

--- lib/config/routes/app_routes.dart
+++ lib/config/routes/app_routes.dart
@@ -1,28 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:instagram/core/utility/constant.dart';
 import 'package:instagram/presentation/screens/web_screen_layout.dart';
 
 /// currently, I don't use routes methods because there is a lot of run time errors.
 /// I use normal Navigator because i don't know how to make Get.to without root.
-Future pushToPage(
-  BuildContext context, {
-  required Widget page,
-  bool withoutRoot = true,
-  bool withoutPageTransition = false,
-  bool pageRoute = false,
-}) async {
-  if (isThatMobile || pageRoute) {
-    PageRoute route = withoutPageTransition
-        ? MaterialPageRoute(
-            builder: (context) => page, maintainState: !withoutRoot)
-        : CupertinoPageRoute(
-            builder: (context) => page, maintainState: !withoutRoot);
-    return Navigator.of(context, rootNavigator: withoutRoot).push(route);
-  } else {
-    PageRoute route = MaterialPageRoute(
-        builder: (context) => WebScreenLayout(body: page),
-        maintainState: !withoutRoot);
-    return Navigator.of(context, rootNavigator: withoutRoot).push(route);
+class Go {
+  final BuildContext context;
+  Go(this.context);
+
+  Future push({
+    required Widget page,
+    bool withoutRoot = true,
+    bool withoutPageTransition = false,
+  }) async {
+    if (isThatMobile) {
+      PageRoute route = withoutPageTransition
+          ? MaterialPageRoute(
+              builder: (context) => page, maintainState: !withoutRoot)
+          : CupertinoPageRoute(
+              builder: (context) => page, maintainState: !withoutRoot);
+      return Navigator.of(context, rootNavigator: withoutRoot).push(route);
+    } else {
+      return Get.to(WebScreenLayout(body: page),
+          transition: Transition.noTransition,
+          duration: const Duration(milliseconds: 0));
+    }
   }
 }
