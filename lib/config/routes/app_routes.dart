79fce4882d53d7802cb04f59c3d42b6f3b7490a9
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-01-25 19:46:22 +0200

    restructure the page routes

--- lib/config/routes/app_routes.dart
+++ lib/config/routes/app_routes.dart
@@ -1,28 +1,46 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram/core/utility/constant.dart';
-import 'package:instagram/presentation/screens/web_screen_layout.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:instagram/core/resources/strings_manager.dart';
+class Routes {
+  static const String storyPageForMobile = "/story Page";
+  static const String followersInfoPage = "/Followers Info";
+  static const String usersWhoLikesForMobile = "/Users Who Likes";
+  static const String createStoryPage = "/Create Story";
+  static const String getsPostInfoAndDisplay = "/Gets Post And Display";
+  static const String mainRoute = "/main";
+  static const String storeDetailsRoute = "/storeDetails";
+}
 
-/// currently, I don't use routes methods because there is a lot of run time errors.
-/// I use normal Navigator because i don't know how to make Get.to without root.
-Future pushToPage(
-  BuildContext context, {
-  required Widget page,
-  bool withoutRoot = true,
-  bool withoutPageTransition = false,
-  bool pageRoute = false,
-}) async {
-  if (isThatMobile || pageRoute) {
-    PageRoute route = withoutPageTransition
-        ? MaterialPageRoute(
-            builder: (context) => page, maintainState: !withoutRoot)
-        : CupertinoPageRoute(
-            builder: (context) => page, maintainState: !withoutRoot);
-    return Navigator.of(context, rootNavigator: withoutRoot).push(route);
-  } else {
-    PageRoute route = MaterialPageRoute(
-        builder: (context) => WebScreenLayout(body: page),
-        maintainState: !withoutRoot);
-    return Navigator.of(context, rootNavigator: withoutRoot).push(route);
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.splashRoute:
+        return CupertinoPageRoute(builder: (_) => SplashView());
+      case Routes.loginRoute:
+        return CupertinoPageRoute(builder: (_) => LoginView());
+      case Routes.onBoardingRoute:
+        return CupertinoPageRoute(builder: (_) => OnBoardingView());
+      case Routes.registerRoute:
+        return CupertinoPageRoute(builder: (_) => RegisterView());
+      case Routes.forgotPasswordRoute:
+        return CupertinoPageRoute(builder: (_) => ForgotPasswordView());
+      case Routes.mainRoute:
+        return CupertinoPageRoute(builder: (_) => MainView());
+      case Routes.storeDetailsRoute:
+        return CupertinoPageRoute(builder: (_) => StoreDetailsView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return CupertinoPageRoute(
+        builder: (_) => Scaffold(
+          appBar: AppBar(
+            title: const Text(StringsManager.noRouteFound).tr(),
+          ),
+          body: Center(child: const Text(StringsManager.noRouteFound).tr()),
+        ));
   }
 }
