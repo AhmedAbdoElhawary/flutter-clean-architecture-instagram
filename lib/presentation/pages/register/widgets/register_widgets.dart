443b07129ae2814fae7551f44c91f807e9247555
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-11-10 03:25:56 -0800

    create _EmailTextFields for register

--- lib/presentation/pages/register/widgets/register_widgets.dart
+++ lib/presentation/pages/register/widgets/register_widgets.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -6,6 +7,7 @@ import 'package:instagram/core/resources/color_manager.dart';
 import 'package:instagram/core/resources/strings_manager.dart';
 import 'package:instagram/core/resources/styles_manager.dart';
 import 'package:instagram/core/utility/constant.dart';
+import 'package:instagram/presentation/cubit/firebaseAuthCubit/firebase_auth_cubit.dart';
 import 'package:instagram/presentation/pages/register/sign_up_page.dart';
 import 'package:instagram/presentation/pages/register/widgets/or_text.dart';
 import 'package:instagram/presentation/widgets/global/custom_widgets/custom_text_field.dart';
@@ -103,10 +105,9 @@ class _SignUpPageState extends State<RegisterWidgets> {
             height: 50,
           ),
           const SizedBox(height: 30),
-          CustomTextField(
+          _EmailTextFields(
             hint: StringsManager.email.tr,
             controller: widget.emailController,
-            isThatEmail: true,
             validate: widget.validateEmail,
             isThatLogin: widget.isThatLogIn,
           ),
@@ -237,6 +238,110 @@ class _SignUpPageState extends State<RegisterWidgets> {
     return Text(
       widget.isThatLogIn ? StringsManager.signUp.tr : StringsManager.logIn.tr,
       style: getBoldStyle(fontSize: 13, color: ColorManager.blue),
+    );
+  }
+}
+
+class _EmailTextFields extends StatefulWidget {
+  final TextEditingController controller;
+  final String hint;
+  final ValueNotifier<bool>? validate;
+  final bool isThatLogin;
+  const _EmailTextFields(
+      {required this.controller,
+      required this.hint,
+      required this.isThatLogin,
+      this.validate,
+      Key? key})
+      : super(key: key);
+
+  @override
+  State<_EmailTextFields> createState() => _EmailTextFieldsState();
+}
+
+class _EmailTextFieldsState extends State<_EmailTextFields> {
+  String? errorMassage;
+  @override
+  void initState() {
+    widget.controller.addListener(() {
+      if (widget.controller.text.isNotEmpty) {
+        errorMassage = _validateEmail();
+      } else {
+        errorMassage = null;
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: SizedBox(
+        height: isThatMobile ? null : 37,
+        width: double.infinity,
+        child: BlocConsumer<FirebaseAuthCubit, FirebaseAuthCubitState>(
+          bloc: FirebaseAuthCubit.get(context)
+            ..isThisEmailToken(email: widget.controller.text),
+          listenWhen: (previous, current) =>
+              previous != current && current is CubitEmailVerificationLoaded,
+          listener: (context, state) {
+            if (!widget.isThatLogin) {
+              if (state is CubitEmailVerificationLoaded &&
+                  state.isThisEmailToken) {
+                errorMassage = "This email already exists.";
+                widget.validate?.value = false;
+              } else {
+                errorMassage = null;
+                widget.validate?.value = true;
+              }
+            }
+          },
+          buildWhen: (previous, current) =>
+              previous != current && current is CubitEmailVerificationLoaded,
+          builder: (context, state) {
+            return TextFormField(
+              controller: widget.controller,
+              cursorColor: ColorManager.teal,
+              style: getNormalStyle(
+                  color: Theme.of(context).focusColor, fontSize: 15),
+              decoration: InputDecoration(
+                hintText: widget.hint,
+                hintStyle: isThatMobile
+                    ? getNormalStyle(color: Theme.of(context).indicatorColor)
+                    : getNormalStyle(color: ColorManager.black54, fontSize: 12),
+                fillColor: const Color.fromARGB(48, 232, 232, 232),
+                filled: true,
+                focusedBorder: outlineInputBorder(),
+                enabledBorder: outlineInputBorder(),
+                errorStyle: getNormalStyle(color: ColorManager.red),
+                errorText: isThatMobile ? errorMassage : null,
+                contentPadding: EdgeInsets.symmetric(
+                    horizontal: 10, vertical: isThatMobile ? 15 : 5),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  String? _validateEmail() {
+    if (!widget.controller.text.isEmail) {
+      setState(() => widget.validate!.value = false);
+      return 'Please make sure your email address is valid';
+    } else {
+      setState(() => widget.validate!.value = true);
+      return null;
+    }
+  }
+
+  OutlineInputBorder outlineInputBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(isThatMobile ? 5.0 : 1.0),
+      borderSide: BorderSide(
+          color: ColorManager.lightGrey, width: isThatMobile ? 1.0 : 0.8),
     );
   }
 }
