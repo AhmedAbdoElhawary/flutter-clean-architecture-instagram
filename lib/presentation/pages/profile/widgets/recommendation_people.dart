39ac6fc83746ffa0764a37de23a984920d014e32
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-08-27 22:08:31 +0300

    handle new version key of widget

--- lib/presentation/pages/profile/widgets/recommendation_people.dart
+++ lib/presentation/pages/profile/widgets/recommendation_people.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class RecommendationPeople extends StatelessWidget {
-  const RecommendationPeople({Key? key}) : super(key: key);
+  const RecommendationPeople({super.key});
 
   @override
   Widget build(BuildContext context) {
