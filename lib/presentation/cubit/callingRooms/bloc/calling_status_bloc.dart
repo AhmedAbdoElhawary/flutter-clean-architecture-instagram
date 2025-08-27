0619674c02825da608b169392efe46603fa58982
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-08-27 22:34:25 +0300

    handle Bloc with new flutter version

--- lib/presentation/cubit/callingRooms/bloc/calling_status_bloc.dart
+++ lib/presentation/cubit/callingRooms/bloc/calling_status_bloc.dart
@@ -9,35 +9,32 @@ part 'calling_status_state.dart';
 class CallingStatusBloc extends Bloc<CallingStatusEvent, CallingStatusState> {
   final GetCallingStatusUseCase _getCallingStatusUseCase;
 
-  CallingStatusBloc(this._getCallingStatusUseCase)
-      : super(CallingStatusInitial());
-
-  static CallingStatusBloc get(BuildContext context) =>
-      BlocProvider.of(context);
-
-  @override
-  Stream<CallingStatusState> mapEventToState(
-    CallingStatusEvent event,
-  ) async* {
-    if (event is LoadCallingStatus) {
-      yield* _mapLoadInfoToState(event.channelUid);
-    } else if (event is UpdateCallingStatus) {
-      yield* _mapUpdateInfoToState(event);
-    }
+  CallingStatusBloc(this._getCallingStatusUseCase) : super(CallingStatusInitial()) {
+    on<LoadCallingStatus>(_onLoadCallingStatus);
+    on<UpdateCallingStatus>(_onUpdateCallingStatus);
   }
 
-  Stream<CallingStatusState> _mapLoadInfoToState(String channelUid) async* {
-    _getCallingStatusUseCase.call(params: channelUid).listen(
-      (isHeOnLine) {
+  static CallingStatusBloc get(BuildContext context) => BlocProvider.of(context);
+
+  Future<void> _onLoadCallingStatus(
+    LoadCallingStatus event,
+    Emitter<CallingStatusState> emit,
+  ) async {
+    await emit.forEach<bool>(
+      _getCallingStatusUseCase.call(params: event.channelUid),
+      onData: (isHeOnLine) {
+        // also dispatch UpdateCallingStatus event to keep two-step flow
         add(UpdateCallingStatus(isHeOnLine));
+        return CallingStatusLoaded(callingStatus: isHeOnLine);
       },
-    ).onError((e) async* {
-      yield CallingStatusFailed(e.toString());
-    });
+      onError: (e, _) => CallingStatusFailed(e.toString()),
+    );
   }
 
-  Stream<CallingStatusState> _mapUpdateInfoToState(
-      UpdateCallingStatus event) async* {
-    yield CallingStatusLoaded(callingStatus: event.callingStatus);
+  void _onUpdateCallingStatus(
+    UpdateCallingStatus event,
+    Emitter<CallingStatusState> emit,
+  ) {
+    emit(CallingStatusLoaded(callingStatus: event.callingStatus));
   }
 }
